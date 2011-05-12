3e08eeea189e36bd47941599dccf43fd63cea937
smancini <smancini@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2011-05-12 06:27:43 +0000

    Delete of the previous matlab acorn_summary_GBR_SAG_PCY.m
    git-svn-id: https://svn.emii.org.au/repos/acorn/trunk@3893 80f082c3-aa3d-4a8a-bc4c-16289f4b763f

--- acorn_summary_GBR_SAG_PCY.m
+++
@@ -1,9 +0,0 @@
-try
-acorn_listing_main_netcdf_UNIX('GBR','GBR_SQL_last_update.txt')
-end
-try
-acorn_listing_main_netcdf_UNIX('SAG','SAG_SQL_last_update.txt')
-end
-try
-acorn_listing_main_netcdf_UNIX('PCY','PCY_SQL_last_update.txt')
-end
