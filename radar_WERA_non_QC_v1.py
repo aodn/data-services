21ec753f536dbe1b54b6a892597b97b850c9ea63
smancini <smancini@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2011-05-20 08:47:20 +0000

    add python script to connect to the DataFabric, run the matlab script and copy netcdf files to the DataFabric
    git-svn-id: https://svn.emii.org.au/repos/acorn/trunk@3926 80f082c3-aa3d-4a8a-bc4c-16289f4b763f

---
+++ radar_WERA_non_QC_v1.py
@@ -0,0 +1,39 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+import os,sys,threading
+import DatafabricConnection,folderCopier
+
+if __name__ == "__main__":
+
+
+   
+    df = DatafabricConnection.DatafabricConnection()
+     
+    print "Checking the datafabric.."
+    if df.connectDatafabric():
+       print "Datafabric now connected"
+       try:
+           os.system("/usr/local/bin/matlab -nodisplay -r  \"run ('/usr/local/harvesters/matlab_3/svn/acorn/trunk/acorn_summary_CBG_SAG_ROT.m')\"")
+       except Exception, e:
+           print ("ERROR: " + str(e))     
+
+       f = folderCopier.folderCopier()
+
+       f.processFiles("/var/lib/matlab_3/ACORN/WERA/radial_nonQC/output/datafabric/gridded_1havg_currentmap_nonQC/CBG","/home/matlab_3/datafabric_root/opendap/ACORN/gridded_1h-avg-current-map_non-QC/CBG",'nc')
+       f.processFiles("/var/lib/matlab_3/ACORN/WERA/radial_nonQC/output/datafabric/gridded_1havg_currentmap_nonQC/SAG","/home/matlab_3/datafabric_root/opendap/ACORN/gridded_1h-avg-current-map_non-QC/SAG",'nc')
+       f.processFiles("/var/lib/matlab_3/ACORN/WERA/radial_nonQC/output/datafabric/gridded_1havg_currentmap_nonQC/ROT","/home/matlab_3/datafabric_root/opendap/ACORN/gridded_1h-avg-current-map_non-QC/ROT",'nc')
+
+       f.close()
+          
+    # disconnect using a thread
+    def doit():            
+        if df.isDfMounted():
+            df.unconnectDatafabric()
+            
+    print "Disconnecting the datafabric.."    
+    t = threading.Timer(60.0, doit)
+    t.start()
+    
+
+   
+
