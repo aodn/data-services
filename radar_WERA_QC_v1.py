a8255d73e484abf7a6b1a37442363f11a759314d
smancini <smancini@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2011-07-20 01:04:17 +0000

    Add matlab and python scripts in order to convert radial quality controlled data to 1 hour average gridded product.
    git-svn-id: https://svn.emii.org.au/repos/acorn/trunk@4228 80f082c3-aa3d-4a8a-bc4c-16289f4b763f

---
+++ radar_WERA_QC_v1.py
@@ -0,0 +1,39 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+import os,sys,threading
+import DatafabricConnection,folderCopier
+
+if __name__ == "__main__":
+
+
+   
+    df = DatafabricConnection.DatafabricConnection()
+     
+    print "Checking the datafabric.."
+    if df.connectDatafabric():
+       print "Datafabric now connected"
+       try:
+           os.system("/usr/local/bin/matlab -nodisplay -r  \"run ('/usr/local/harvesters/matlab_3/svn/acorn/trunk/acorn_summary_WERA_QC.m')\"")
+       except Exception, e:
+           print ("ERROR: " + str(e))     
+
+       f = folderCopier.folderCopier()
+
+       f.processFiles("/var/lib/matlab_3/ACORN/WERA/radial_QC/output/datafabric/gridded_1havg_currentmap_QC/CBG","/home/matlab_3/datafabric_root/opendap/ACORN/gridded_1h-avg-current-map_QC/CBG",'nc')
+       f.processFiles("/var/lib/matlab_3/ACORN/WERA/radial_QC/output/datafabric/gridded_1havg_currentmap_QC/SAG","/home/matlab_3/datafabric_root/opendap/ACORN/gridded_1h-avg-current-map_QC/SAG",'nc')
+       f.processFiles("/var/lib/matlab_3/ACORN/WERA/radial_QC/output/datafabric/gridded_1havg_currentmap_QC/ROT","/home/matlab_3/datafabric_root/opendap/ACORN/gridded_1h-avg-current-map_QC/ROT",'nc')
+
+       f.close()
+          
+    # disconnect using a thread
+    def doit():            
+        if df.isDfMounted():
+            df.unconnectDatafabric()
+            
+    print "Disconnecting the datafabric.."    
+    t = threading.Timer(60.0, doit)
+    t.start()
+    
+
+   
+
