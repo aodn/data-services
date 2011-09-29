74b3302625eae9c041b6d027bad2159c3a455358
mhidas <mhidas@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2011-09-29 07:33:50 +0000

    started on IMOSNetCDFFile class
    git-svn-id: https://svn.emii.org.au/repos/anmn/trunk@4776 80f082c3-aa3d-4a8a-bc4c-16289f4b763f

--- IMOSfile/IMOSnetCDF.py
+++ IMOSfile/IMOSnetCDF.py
@@ -4,10 +4,34 @@
 #
 # 2011  Marton Hidas 
 
+import Scientific.IO.NetCDF as nc
 
-# Required functionality:
 
-# create a new file, pre-filling standard global attributes.
+class IMOSNetCDFFile(nc.NetCDFFile):
+    """
+    netCDF file following the IMOS netCDF conventions.
+
+    Based on the IMOS NetCDF User Manual (v1.2) and File Naming
+    Convention (v1.3), which can be obtained from
+    http://imos.org.au/facility_manuals.html
+    """
+
+    def __init__(self, filename):
+        """
+        Create a new empty file, pre-filling some mandatory global attributes.
+        """
+
+        # Open the file
+        nc.NetCDFFile.__init__(self, filename, 'w')
+
+        # Create mandatory global attributes
+        self.project = 'Integrated Marine Observing System (IMOS)'
+        self.conventions = 'IMOS version 1.2'
+        self.data_centre = 'eMarine Information Infrastructure (eMII)'
+        self.data_centre_email = 'info@emii.org.au'
+
+
+# Functionality to be added:
 
 # add attributes and data to an open file (auto-filling as many of the
 # attributes as possible)
