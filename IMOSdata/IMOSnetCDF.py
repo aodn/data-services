e90b83d01cd244343ce4414b48d9b00b0eb0c8a9
mhidas <mhidas@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2011-09-29 06:25:33 +0000

    New package to handle IMOS data formats and conventions.
    git-svn-id: https://svn.emii.org.au/repos/anmn/trunk@4771 80f082c3-aa3d-4a8a-bc4c-16289f4b763f

---
+++ IMOSdata/IMOSnetCDF.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+#
+# Python module to manage IMOS-standard netCDF data files.
+#
+# 2011  Marton Hidas 
+
+
+# Required functionality:
+
+# create a new file, pre-filling standard global attributes.
+
+# add attributes and data to an open file (auto-filling as many of the
+# attributes as possible)
+
+# check that a given file is written according to the IMOS convention,
+
+# check that the filename meets the convention and matches information
+# within the file.
+
