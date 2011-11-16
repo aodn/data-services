c877c66465263abcb7f3d6e5877054e7870b30e2
mhidas <mhidas@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2011-11-16 07:38:36 +0000

    add module to parse and create IMOS standard filenames
    git-svn-id: https://svn.emii.org.au/repos/anmn/trunk@5129 80f082c3-aa3d-4a8a-bc4c-16289f4b763f

---
+++ IMOSfile/IMOSfilename.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+#
+# Python module to parse, check and create filenames according to the
+# IMOS convention.
+
+
+# Required functionality:
+
+### Parse a filename string and extract information within it
+
+### Check whether the filename meets the IMOS convention
+
+### Given the relevant information for a data set, create the correct filename
+
