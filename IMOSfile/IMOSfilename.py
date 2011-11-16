b55e1c11bb690465d828a05b356e1b831d0a7a07
mhidas <mhidas@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2011-11-16 08:20:29 +0000

    started implementing parseFilename()
    git-svn-id: https://svn.emii.org.au/repos/anmn/trunk@5130 80f082c3-aa3d-4a8a-bc4c-16289f4b763f

--- IMOSfile/IMOSfilename.py
+++ IMOSfile/IMOSfilename.py
@@ -4,11 +4,73 @@
 # IMOS convention.
 
 
-# Required functionality:
+#### DATA ###################################################################
 
-### Parse a filename string and extract information within it
+# List of valid facility/sub-facility codes.
+subFacilities = ('ANMN-NRS', 
+                 'ANMN-NSW', 
+                 'ANMN-QLD', 
+                 'ANMN-SA', 
+                 'ANMN-WA', 
+                 'ANMN-PA')
 
-### Check whether the filename meets the IMOS convention
+
+#### FUNCTIONS ##############################################################
+
+def parseTime(t):
+    """
+    Parse a date/time string within the filename and convert it into a
+    datetime object.
+    *** TO BE IMPLEMENTED ***
+    """
+    return t
+
+
+def parseFilename(filename):
+    """
+    Parse a filename string, check that it meets the IMOS convention
+    and return the information contained in it.
+    """
+    info = {}
+    errors = []
+
+    # remove file extension
+    extp = filename.rfind('.')
+    if extp >= 0 and extp < len(filename)-1:
+        info['extension'] = filename[extp+1:]
+    else:
+        errors.append('No file extension.')
+
+    # split the string into fields and check the value of each field is valid
+    field = filename[:extp].split('_')
+    if len(field) < 6: 
+        errors.append('Less than 6 fields filename.')
+
+    # project name   
+    if field[0] == 'IMOS':
+        info['project'] = field[0]
+    else:
+        errors.append('Unknown project "'+field[0]+'".')
+
+    # facility and sub-facility
+    if field[1] in subFacilities:
+        info['facility'] = field[1]
+    else:
+        errors.append('Unknonwn sub-facility "'+field[1]+'".')
+
+    # data codes
+    info['data-code'] = field[2]
+
+    # start date/time
+    tt = parseTime(field[3])
+    if tt:
+        info['start-time'] = tt
+    else:
+        errors.append('Invalid start time "'+field[3]+'".')
+
+    # return the values
+    return info, errors
+
 
 ### Given the relevant information for a data set, create the correct filename
 
