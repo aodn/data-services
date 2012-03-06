024a7fb483ae98f967e3b0f89809ae6d6779d048
mhidas <mhidas@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2012-03-06 07:14:41 +0000

    rearranged everything again, with the actual processing now in procWave()
    git-svn-id: https://svn.emii.org.au/repos/anmn/trunk@5851 80f082c3-aa3d-4a8a-bc4c-16289f4b763f

--- NRSrealtime/rtWave.py
+++ NRSrealtime/rtWave.py
@@ -8,6 +8,25 @@ from datetime import datetime
 from NRSrealtime.quickCSV import readCSV
 import IMOSfile.IMOSnetCDF as inc
 
+# load default netCDF attributes
+inc.defaultAttributes = inc.attributesFromFile('/home/marty/work/code/NRSrealtime/attributes.txt')  
+
+
+### module variables ###################################################
+
+i = np.int32
+f = np.float64
+formWave = np.dtype( 
+    [('Config ID', i),
+     ('Trans ID', i),
+     ('Record', i),
+     ('Header Index', i),
+     ('Time', 'S24'),
+     ('Sig. Wave Height', f)])
+
+epoch = datetime(1950,1,1)
+
+
 
 ### functions #######################################################
 
@@ -32,50 +51,38 @@ def timeFromString(timeStr, epoch):
 
 
 
-### module variables ###################################################
-i = np.int32
-f = np.float64
-format =  [('Config ID', i),
-           ('Trans ID', i),
-           ('Record', i),
-           ('Header Index', i),
-           ('Time', 'S24'),
-           ('Sig. Wave Height', f)]
-formWave = np.dtype(format)
+def procWave(csvFile='Wave.csv', ncFile='Wave.nc'):
+    """
+    Read data from a Wave.csv file (in current directory, unless
+    otherwise specified) and convert it to a netCDF file (Wave.nc by
+    default).
+    """
+    
+    # read in Wave file
+    data = readCSV(csvFile, formWave)
 
-csvFile = 'Wave.csv'
-ncFile = 'Wave.nc'
-epoch = datetime(1950,1,1)
+    # convert time from string to something more numeric
+    (time, dtime) = timeFromString(data['Time'], epoch)
+    waveh = data['Sig. Wave Height']
 
+    # create netCDF file
+    file = inc.IMOSnetCDFFile(ncFile)
+    file.title = 'Real-time data from NRSMAI: significant wave height'
 
-### processing ##########################################################
+    TIME = file.setDimension('TIME', time)
+    
+    VAVH = file.setVariable('VAVH', waveh, ('TIME',))
+    # VAVH._FillValue = ???
 
-# read in Wave file
-data = readCSV(csvFile, formWave)
-
-# convert time from string to something more numeric
-(time, dtime) = timeFromString(data['Time'], epoch)
-waveh = data['Sig. Wave Height']
-
-
-# Filter data and time arrays to the time range we want
-# ...
+    file.close()
 
 
-# load default netCDF attributes
-inc.defaultAttributes = inc.attributesFromFile('/home/marty/work/code/NRSrealtime/attributes.txt')  
 
-# create netCDF file
-file = inc.IMOSnetCDFFile(ncFile)
-file.title = 'Real-time data from NRSMAI: significant wave height'
+### processing - if run from command line
 
-TIME = file.setDimension('TIME', time)
-
-VAVH = file.setVariable('VAVH', waveh, ('TIME',))
-#VAVH._FillValue = ???
+if __name__=='__main__':
+    procWave()
 
 
-file.close()
 
 
-# create plots
