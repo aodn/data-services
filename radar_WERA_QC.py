5426f9e667ea4cf65eb24d5469d296eca7ced012
ggalibert <ggalibert@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2012-09-05 04:07:05 +0000

    git-svn-id: https://svn.emii.org.au/repos/acorn/trunk@7750 80f082c3-aa3d-4a8a-bc4c-16289f4b763f

--- radar_WERA_QC.py
+++ radar_WERA_QC.py
@@ -16,14 +16,14 @@ if __name__ == "__main__":
            os.system("/usr/local/bin/matlab -nodisplay -r  \"cd '/usr/local/harvesters/matlab_3/svn/acorn/trunk'; addpath(fullfile('.','Util')); acorn_summary('WERA', true)\"")
        except Exception, e:
            print ("ERROR: " + str(e))
-		   sys.exit()
+           sys.exit()
 
        f = folderCopier.folderCopier()
 
        f.processFiles("/var/lib/matlab_3/ACORN/WERA/radial_QC/output/datafabric/gridded_1havg_currentmap_QC/CBG","/home/matlab_3/datafabric_root/opendap/ACORN/gridded_1h-avg-current-map_QC/CBG",'nc')
        f.processFiles("/var/lib/matlab_3/ACORN/WERA/radial_QC/output/datafabric/gridded_1havg_currentmap_QC/SAG","/home/matlab_3/datafabric_root/opendap/ACORN/gridded_1h-avg-current-map_QC/SAG",'nc')
        f.processFiles("/var/lib/matlab_3/ACORN/WERA/radial_QC/output/datafabric/gridded_1havg_currentmap_QC/ROT","/home/matlab_3/datafabric_root/opendap/ACORN/gridded_1h-avg-current-map_QC/ROT",'nc')
-	   f.processFiles("/var/lib/matlab_3/ACORN/WERA/radial_QC/output/datafabric/gridded_1havg_currentmap_QC/COF","/home/matlab_3/datafabric_root/opendap/ACORN/gridded_1h-avg-current-map_QC/COF",'nc')
+       f.processFiles("/var/lib/matlab_3/ACORN/WERA/radial_QC/output/datafabric/gridded_1havg_currentmap_QC/COF","/home/matlab_3/datafabric_root/opendap/ACORN/gridded_1h-avg-current-map_QC/COF",'nc')
 
        f.close()
           
