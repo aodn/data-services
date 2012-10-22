c095541cbe77748284fe0a7e842b846b8f1c634c
mhidas <mhidas@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2012-10-22 04:41:05 +0000

    added site codes and matched output fields to db columns
    git-svn-id: https://svn.emii.org.au/repos/anmn/trunk@8310 80f082c3-aa3d-4a8a-bc4c-16289f4b763f

--- acoustic/readMetadata.py
+++ acoustic/readMetadata.py
@@ -4,6 +4,49 @@
 
 import re
 
+
+# convert Rob's "Set details" field into site code, deployment name, logger id, frequency and position
+def setDetails(value):
+    
+    code = name = logID = freq = pos = ''
+
+    m = re.findall('(\D+\s*[-\d]+)\s*(.*)', value)
+    if m:
+        name = m[0][0]
+        therest = m[0][1]
+        if re.match('Perth Canyon', name):
+            name = re.sub('Perth Canyon', 'Perth Canyon, WA', name)
+            code = 'PAPCA'
+        elif re.match('Portland', name):
+            name = re.sub('Portland( IMOS)?', 'Portland, VIC', name)
+            code = 'PAPOR'
+        elif re.match('NSW', name):
+            name = re.sub('NSW', 'Sydney, NSW', name)
+            code = 'PASYD'
+        name = re.sub(' (\d{2})-(\d{2}\D?)', ' 20\\1-20\\2', name)
+    else: 
+        therest = value
+
+    m = re.findall('E\d+', therest)
+    if m: 
+        logID = m[0]
+        therest = therest.replace(logID, '')
+
+    m = re.findall('(\d+)\s*k', therest)
+    if m: 
+        freq = m[0]
+        therest = therest.replace(freq, '')
+
+    m = re.findall('P\d+', therest)
+    if m: pos = m[0]
+
+    return code, name, logID, freq, pos
+
+
+
+
+### Main ##################################################################
+
 metafile = 'meta_all.txt'
 F = open(metafile)
 
@@ -21,13 +64,8 @@ for line in F:
     value = line[40:].strip()
 
     if re.match('Set details', field):
-        m = re.findall('(\D+)\s*([-\d]+)\s*(.*)\s+(\d+)\s*k', value)
-        if m:
-            rowDict['site'] = m[0][0]
-            rowDict['year'] = m[0][1]
-            rowDict['loggerID'] = m[0][2]
-            rowDict['freq'] = m[0][3]
-            continue
+        rowDict['site code'], rowDict['deployment name'], rowDict['loggerID'], rowDict['freq'], rowDict['position'] = setDetails(value) 
+        continue
 
     if re.match('Latitude', field) or re.match('Longitude', field):
         m = re.findall("(\S+)\s*deg\s*(\S+)'", value)
@@ -45,18 +83,21 @@ F.close()
 
 # now print out what we want
 printFields = ['Curtin ID',
-               'site', 
-               'year', 
+               'site code',
                'loggerID', 
+               'deployment name', 
+               'primary?',
+               'start first sample (UTC)',
+               'start last sample (UTC)',
+               'data path',
                'freq',
+               'position',
                'Set success',
                'GBytes',
                'Latitude (WGS84)',
                'Longitude (WGS84)',
                'water depth (m)',
                'receiver depth (m)',
-               'start first sample (UTC)',
-               'start last sample (UTC)',
                'start first sample in water (UTC)',
                'start last sample in water (UTC)',
                'first/last sample in water (# samp)',
