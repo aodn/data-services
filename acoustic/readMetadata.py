d8279f8a3ef9b0ddb37ac335cd4456b5c3e3fcce
mhidas <mhidas@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2012-10-22 01:31:21 +0000

    this works better
    git-svn-id: https://svn.emii.org.au/repos/anmn/trunk@8308 80f082c3-aa3d-4a8a-bc4c-16289f4b763f

--- acoustic/readMetadata.py
+++ acoustic/readMetadata.py
@@ -7,32 +7,78 @@ import re
 metafile = 'meta_all.txt'
 F = open(metafile)
 
-ln = 0
-fields = ''
-values = ''
+# read in all records into a list of dictionaries
+rowDict = {}
+allRows = []
 for line in F:
+
     if re.match('\s*$', line):    # empty line -> end of record
-        if ln==0: print fields
-        print values
-        fields = ''
-        values = ''
-        ln += 1
+        allRows.append(rowDict)
+        rowDict = {}
         continue
-    if re.match('elpased days', line): continue # don't need this field
-    if re.match('Set details', line):
-        m = re.findall('(\D+)\s*([-\d]+)\s*(.*)\s+(\d+)\s*k', line[40:].strip())
+
+    field = line[:40].strip()
+    value = line[40:].strip()
+
+    if re.match('Set details', field):
+        m = re.findall('(\D+)\s*([-\d]+)\s*(.*)\s+(\d+)\s*k', value)
         if m:
-            fields += 'site,year,loggerID,freq (kHz),'
-            site, yr, logid, freq = m[0]
-            values += site+','+yr+','+logid+','+freq+','
+            rowDict['site'] = m[0][0]
+            rowDict['year'] = m[0][1]
+            rowDict['loggerID'] = m[0][2]
+            rowDict['freq'] = m[0][3]
             continue
-    if re.match('Latitude', line) or re.match('Longitude', line):
-        m = re.findall("(\S+)\s*deg\s*(\S+)'", line)
+
+    if re.match('Latitude', field) or re.match('Longitude', field):
+        m = re.findall("(\S+)\s*deg\s*(\S+)'", value)
         if m:
-            fields += line[:40].strip()+','
-            val = float(m[0][0]) + float(m[0][1])/60
-            values += str(val)+','
+            value = float(m[0][0]) + float(m[0][1])/60
+
+    try: value = float(value)
+    except: pass
+
+    rowDict[field] = value
+
+F.close()
+
+
+
+# now print out what we want
+printFields = ['Curtin ID',
+               'site', 
+               'year', 
+               'loggerID', 
+               'freq',
+               'Set success',
+               'GBytes',
+               'Latitude (WGS84)',
+               'Longitude (WGS84)',
+               'water depth (m)',
+               'receiver depth (m)',
+               'start first sample (UTC)',
+               'start last sample (UTC)',
+               'start first sample in water (UTC)',
+               'start last sample in water (UTC)',
+               'first/last sample in water (# samp)',
+               'mean sample length/increment (s/min)',
+               'system gain file',
+               'hydrophone serial number',
+               'hydrophone sensitivity (dB re V^2/Pa^2)',
+               'notes']
+
+print ','.join(printFields)
+for rowDict in allRows:
+    values = []
+    for field in printFields:
+        if not rowDict.has_key(field):
+            values.append('')
             continue
-    fields += '"'+line[:40].strip()+'",'
-    values += '"'+line[40:].strip()+'",'
+        value = rowDict[field]
+        if type(value) == str:
+            values.append('"' + value + '"')
+        else:
+            values.append(str(value))
+
+    print ','.join(values)
+
 
