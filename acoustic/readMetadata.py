55f5c23ddb95166e465fd771d07299ed7bda8395
mhidas <mhidas@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2012-10-22 00:38:13 +0000

    current version - works ok
    git-svn-id: https://svn.emii.org.au/repos/anmn/trunk@8305 80f082c3-aa3d-4a8a-bc4c-16289f4b763f

--- acoustic/readMetadata.py
+++ acoustic/readMetadata.py
@@ -4,47 +4,35 @@
 
 import re
 
-
-fields = ['Curtin ID',
-          'Set details',
-          'Set success',
-          'GBytes',
-          'Latitude (WGS84)',
-          'Longitude (WGS84)',
-          'water depth (m)',
-          'receiver depth (m)',
-          'start first sample (UTC)',
-          'start last sample (UTC)',
-          'start first sample in water (UTC)',
-          'start last sample in water (UTC)',
-          'elpased days',
-          'first/last sample in water (# samp)',
-          'mean sample length/increment (s/min)',
-          'system clock set 1',
-          'system clock set 2',
-          'clock drift (s) (s/day)',
-          'system gain file',
-          'hydrophone serial number',
-          'hydrophone sensitivity (dB re V^2/Pa^2)',
-          'notes'
-]
-nfields = len(fields)
-
-
-# metafile = 'IMOS_NSW_2010_2011_PA_MetaData.txt'
-metafile = 'IMOS_PassiveAcoustics_20110401.txt'
+metafile = 'meta_all.txt'
 F = open(metafile)
 
-
-f = 0
-row = ''
+ln = 0
+fields = ''
+values = ''
 for line in F:
-    if re.match('\s*$', line): continue   # empty line
-    value = re.findall(fields[f]+'\s+(.*\S)\s*', line)
-    if value: row += value[0]
-    row += ','
-    f = (f+1) % nfields
-    if f==0: 
-        print row
-        row = ''
+    if re.match('\s*$', line):    # empty line -> end of record
+        if ln==0: print fields
+        print values
+        fields = ''
+        values = ''
+        ln += 1
+        continue
+    if re.match('elpased days', line): continue # don't need this field
+    if re.match('Set details', line):
+        m = re.findall('(\D+)\s*([-\d]+)\s*(.*)\s+(\d+)\s*k', line[40:].strip())
+        if m:
+            fields += 'site,year,loggerID,freq (kHz),'
+            site, yr, logid, freq = m[0]
+            values += site+','+yr+','+logid+','+freq+','
+            continue
+    if re.match('Latitude', line) or re.match('Longitude', line):
+        m = re.findall("(\S+)\s*deg\s*(\S+)'", line)
+        if m:
+            fields += line[:40].strip()+','
+            val = float(m[0][0]) + float(m[0][1])/60
+            values += str(val)+','
+            continue
+    fields += '"'+line[:40].strip()+'",'
+    values += '"'+line[40:].strip()+'",'
 
