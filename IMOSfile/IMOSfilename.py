d442dfb782cbf4bd5e15fe12ba0d6891831704ff
mhidas <mhidas@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2012-12-19 01:25:59 +0000

    completed basic functionality for parseFilename and parseTime
    git-svn-id: https://svn.emii.org.au/repos/anmn/trunk@8634 80f082c3-aa3d-4a8a-bc4c-16289f4b763f

--- IMOSfile/IMOSfilename.py
+++ IMOSfile/IMOSfilename.py
@@ -4,6 +4,10 @@
 # IMOS convention.
 
 
+import re
+from datetime import datetime
+
+
 #### DATA ###################################################################
 
 # List of valid facility/sub-facility codes.
@@ -17,22 +21,28 @@ subFacilities = ('ANMN-NRS',
 
 #### FUNCTIONS ##############################################################
 
-def parseTime(t):
+def parseTime(tStr):
     """
     Parse a date/time string within the filename and convert it into a
     datetime object.
-    *** TO BE IMPLEMENTED ***
+    *** INCOMPLETE!!! ***
     """
-    return t
+    try:
+        dt = datetime.strptime(tStr, '%Y%m%dT%H%M%SZ')
+    except:
+        return None
+
+    return dt
 
 
-def parseFilename(filename):
+def parseFilename(filename, minFields=6):
     """
     Parse a filename string, check that it meets the IMOS convention
     and return the information contained in it.
     """
-    info = {}
+    info = {'facility':'', 'data-code':'', 'start-time':'', 'site-code':'', 'file-version':'', 'product-code':'', 'deployment-code':'', 'instrument':'', 'end-time':'', 'creation-time':''}
     errors = []
+    minFields = max(minFields, 6)
 
     # remove file extension
     extp = filename.rfind('.')
@@ -43,30 +53,81 @@ def parseFilename(filename):
 
     # split the string into fields and check the value of each field is valid
     field = filename[:extp].split('_')
-    if len(field) < 6: 
-        errors.append('Less than 6 fields filename.')
+    if len(field) < minFields: 
+        errors.append('Less than %d fields in filename.' % minFields)
+        return info, errors
 
-    # project name   
-    if field[0] == 'IMOS':
-        info['project'] = field[0]
-    else:
-        errors.append('Unknown project "'+field[0]+'".')
+        
+    # project name 
+    fld = field.pop(0)
+    if fld <> 'IMOS':
+        errors.append('Unknown project "'+fld+'".')
+
 
     # facility and sub-facility
-    if field[1] in subFacilities:
-        info['facility'] = field[1]
+    fld = field.pop(0)
+    if fld in subFacilities:
+        info['facility'] = fld
     else:
-        errors.append('Unknonwn sub-facility "'+field[1]+'".')
+        errors.append('Unknonwn sub-facility "'+fld+'".')
+
 
     # data codes
-    info['data-code'] = field[2]
+    info['data-code'] = field.pop(0)
+
 
     # start date/time
-    tt = parseTime(field[3])
-    if tt:
-        info['start-time'] = tt
+    fld = field.pop(0)
+    dt = parseTime(fld)
+    if dt:
+        info['start-time'] = dt
     else:
-        errors.append('Invalid start time "'+field[3]+'".')
+        errors.append('Invalid start time "'+fld+'".')
+
+
+    # site code
+    info['site-code'] = field.pop(0)
+
+
+    # file version
+    info['file-version'] = field.pop(0)
+
+
+    # deployment & product code
+    if not field: return info, errors
+    prod = field.pop(0)
+    info['product-code'] = prod
+    m = re.findall('(%s-\d{4})-(.*)' % info['site-code'], prod)
+    if m:
+        info['deployment-code'], info['instrument'] = m[0]
+    else:
+        errors.append('Can\'t extract deployment code from "%s"' % prod)
+
+    # end time
+    if not field: return info, errors
+    fld = field.pop(0)
+    if fld.find('END-') == 0  and  len(fld) > 4:
+        dt = parseTime(fld[4:])
+        if dt:
+            info['end-time'] = dt
+        else:
+            errors.append('Invalid end time "'+fld+'".')
+    else:
+        errors.append('Invalid end time "'+fld+'".')
+
+
+    # creation time
+    if not field: return info, errors
+    fld = field.pop(0)
+    if fld.find('C-') == 0  and  len(fld) > 2:
+        dt = parseTime(fld[2:])
+        if dt:
+            info['creation-time'] = dt
+        else:
+            errors.append('Invalid creation time "'+fld+'".')
+    else:
+        errors.append('Invalid creation time "'+fld+'".')
+
 
     # return the values
     return info, errors
