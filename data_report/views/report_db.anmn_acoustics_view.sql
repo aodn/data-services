1da1769e4e1e70c6a7e43d79decc639ab8022c20
mhidas <mhidas@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2013-01-11 06:58:44 +0000

    Start of view for passive acoustics report
    git-svn-id: https://svn.emii.org.au/repos/anmn/trunk@8668 80f082c3-aa3d-4a8a-bc4c-16289f4b763f

---
+++ data_report/views/report_db.anmn_acoustics_view.sql
@@ -0,0 +1,10 @@
+﻿SELECT 
+  substring(deployment_name from E'\\D+') AS site_name,
+  substring(deployment_name from '2[-0-9]+') AS deployment_year,
+  date(min(time_deployment_start)) AS deployment_start,
+  date(max(time_deployment_end)) AS deployment_end,
+  bool_or(data_path IS NOT NULL) AS data_public
+FROM anmn.acoustic_deployments
+WHERE frequency=6 AND set_success != 'Failure'
+GROUP BY site_name, deployment_year
+ORDER BY site_name, deployment_year
