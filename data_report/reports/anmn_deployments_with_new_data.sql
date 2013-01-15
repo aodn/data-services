cb12fd792418add52a926557355dfbeac49c3a1e
mhidas <mhidas@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2013-01-15 05:12:20 +0000

    now queries the new anmn_deployments_view
    git-svn-id: https://svn.emii.org.au/repos/anmn/trunk@8671 80f082c3-aa3d-4a8a-bc4c-16289f4b763f

--- data_report/reports/anmn_deployments_with_new_data.sql
+++ data_report/reports/anmn_deployments_with_new_data.sql
@@ -1,9 +1,9 @@
-﻿SELECT sub_facility, site, platform_code, deployment_code, 
-       date(min(time_coverage_start)) AS time_coverage_start, 
-       date(max(time_coverage_end)) AS time_coverage_end,
-       date(max(date_public)) AS date_on_portal,
-       count(platform_code) AS number_of_files
-FROM report.anmn_regions_view
-WHERE time_since_public < interval '1 months'
-GROUP BY sub_facility, site, platform_code, deployment_code
-ORDER BY sub_facility, site, platform_code, deployment_code;
+﻿SELECT sub_facility, site_name, deployment_code,  
+			bool_or(date_first_processed <> date_last_processed) AS reprocessed,
+      bool_or(date_first_upload = date_last_upload) AS new_upload, 
+			bool_or(date_first_public = date_last_public) AS new_public,
+			max(date_last_public)
+FROM anmn_deployments_view
+WHERE date_first_public > '2012-12-18' 
+GROUP BY sub_facility, site_name, deployment_code
+ORDER BY sub_facility, site_name, deployment_code;
