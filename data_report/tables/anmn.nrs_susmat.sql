f740da12e109529b2d2247801e03c0287ac8ac9b
mhidas <mhidas@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2013-02-08 01:08:38 +0000

    add sample_number, indexed timestamps and pkey constraint
    git-svn-id: https://svn.emii.org.au/repos/anmn/trunk@8755 80f082c3-aa3d-4a8a-bc4c-16289f4b763f

--- data_report/tables/anmn.nrs_susmat.sql
+++ data_report/tables/anmn.nrs_susmat.sql
@@ -1,26 +1,30 @@
 
--- DROP TABLE anmn.nrs_susmat
+-- DROP TABLE anmn.nrs_susmat;
 
 CREATE TABLE anmn.nrs_susmat
 (
   pkid serial NOT NULL,
   sample_time timestamp with time zone NOT NULL,
   site_code character(6) NOT NULL,
-  sample_lat double precision NOT NULL,
-  sample_lon double precision NOT NULL,
+  sample_lat double precision,
+  sample_lon double precision,
   sample_depth real NOT NULL,
   sample_qc integer,
   sample_comment character varying(255),
+  sample_number integer,
   tss double precision,
   inorganic_fraction double precision,
   organic_fraction double precision,
   secchi_depth double precision,
-  secchi_comment character varying(255)
+  secchi_comment character varying(255),
+  first_indexed timestamp with time zone NOT NULL,
+  last_indexed timestamp with time zone NOT NULL,
+  CONSTRAINT nrs_susmat_pkey PRIMARY KEY (pkid)
 ) 
 WITH (
   OIDS = FALSE
-)
-;
+);
+
 ALTER TABLE anmn.nrs_susmat OWNER TO report;
 COMMENT ON COLUMN anmn.nrs_susmat.sample_time IS 'Time of sample collection';
 COMMENT ON TABLE anmn.nrs_susmat
