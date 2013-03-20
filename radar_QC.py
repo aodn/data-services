3e2cfd072b8fe7334be64e13c543b4491036c9e0
ggalibert <ggalibert@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2013-03-20 06:32:40 +0000

    git-svn-id: https://svn.emii.org.au/repos/acorn/trunk@8838 80f082c3-aa3d-4a8a-bc4c-16289f4b763f

--- radar_QC.py
+++ radar_QC.py
@@ -1,28 +1,13 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
 import os,sys
-import DatafabricConnection,folderCopier
 
 if __name__ == "__main__":
 
-    df = DatafabricConnection.DatafabricConnection()
-
-    if df.isDfMounted():
-       print "Connected to the datafabric"
-       try:
-           os.system("/usr/local/bin/matlab -nodisplay -r  \"cd '/usr/local/harvesters/matlab_3/svn/acorn/trunk'; addpath(fullfile('.', 'Util')); acorn_summary('WERA', true); exit\"")
-       except Exception, e:
-           print ("ERROR: " + str(e))
-           sys.exit()
+    try:
+        os.system("/usr/local/bin/matlab -nodisplay -r  \"cd '/usr/local/bin/ACORN/exp/trunk'; addpath(fullfile('.', 'Util')); acorn_summary('WERA', true); exit\"")
+    except Exception, e:
+        print ("ERROR: " + str(e))
+        sys.exit()
        
-       f = folderCopier.folderCopier()
-
-       f.processFiles("/var/lib/matlab_3/ACORN/WERA/radial_QC/output/datafabric/gridded_1havg_currentmap_QC/CBG/2012", "/home/matlab_3/df_root/opendap/ACORN/gridded_1h-avg-current-map_QC/CBG/2012", 'nc')
-       f.processFiles("/var/lib/matlab_3/ACORN/WERA/radial_QC/output/datafabric/gridded_1havg_currentmap_QC/SAG/2012", "/home/matlab_3/df_root/opendap/ACORN/gridded_1h-avg-current-map_QC/SAG/2012", 'nc')
-       f.processFiles("/var/lib/matlab_3/ACORN/WERA/radial_QC/output/datafabric/gridded_1havg_currentmap_QC/ROT/2012", "/home/matlab_3/df_root/opendap/ACORN/gridded_1h-avg-current-map_QC/ROT/2012", 'nc')
-       f.processFiles("/var/lib/matlab_3/ACORN/WERA/radial_QC/output/datafabric/gridded_1havg_currentmap_QC/COF/2012", "/home/matlab_3/df_root/opendap/ACORN/gridded_1h-avg-current-map_QC/COF/2012", 'nc')
-
-       f.close()
-    else:
-       print "Failed to connect to the datafabric so exiting."
-       sys.exit()
+    os.system("/usr/bin/rsync -aR --remove-source-files /home/ggalibert/DATA/ACORN/WERA/radial_QC/output/datafabric/gridded_1havg_currentmap_QC/./ /mnt/imos-t3/IMOS/opendap/ACORN/gridded_1h-avg-current-map_QC/")
--- radar_non_QC.py
+++ radar_non_QC.py
@@ -1,30 +1,14 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
 import os,sys
-import DatafabricConnection,folderCopier
 
 if __name__ == "__main__":
 
-    df = DatafabricConnection.DatafabricConnection()
-
-    if df.isDfMounted():
-       print "Connected to the datafabric"
-       try:
-           os.system("/usr/local/bin/matlab -nodisplay -r  \"cd '/usr/local/bin/ACORN/exp/trunk'; addpath(fullfile('.', 'Util')); acorn_summary('WERA', false); acorn_summary('CODAR', false); exit\"")
-       except Exception, e:
-           print ("ERROR: " + str(e))
-           sys.exit()
+    try:
+        os.system("/usr/local/bin/matlab -nodisplay -r  \"cd '/usr/local/bin/ACORN/exp/trunk'; addpath(fullfile('.', 'Util')); acorn_summary('WERA', false); acorn_summary('CODAR', false); exit\"")
+    except Exception, e:
+        print ("ERROR: " + str(e))
+        sys.exit()
        
-       f = folderCopier.folderCopier()
-
-       f.processFiles("/var/lib/matlab_3/ACORN/WERA/radial_nonQC/output/datafabric/gridded_1havg_currentmap_nonQC/CBG/2012", "/home/matlab_3/df_root/opendap/ACORN/gridded_1h-avg-current-map_non-QC/CBG/2012", 'nc')
-       f.processFiles("/var/lib/matlab_3/ACORN/WERA/radial_nonQC/output/datafabric/gridded_1havg_currentmap_nonQC/SAG/2012", "/home/matlab_3/df_root/opendap/ACORN/gridded_1h-avg-current-map_non-QC/SAG/2012", 'nc')
-       f.processFiles("/var/lib/matlab_3/ACORN/WERA/radial_nonQC/output/datafabric/gridded_1havg_currentmap_nonQC/ROT/2012", "/home/matlab_3/df_root/opendap/ACORN/gridded_1h-avg-current-map_non-QC/ROT/2012", 'nc')
-       f.processFiles("/var/lib/matlab_3/ACORN/WERA/radial_nonQC/output/datafabric/gridded_1havg_currentmap_nonQC/COF/2012", "/home/matlab_3/df_root/opendap/ACORN/gridded_1h-avg-current-map_non-QC/COF/2012", 'nc')
-       f.processFiles("/var/lib/matlab_3/ACORN/CODAR/nonQC_gridded/output/datafabric/gridded_1havg_currentmap_nonQC/TURQ/2012", "/home/matlab_3/df_root/opendap/ACORN/gridded_1h-avg-current-map_non-QC/TURQ/2012", 'nc')
-       f.processFiles("/var/lib/matlab_3/ACORN/CODAR/nonQC_gridded/output/datafabric/gridded_1havg_currentmap_nonQC/BONC/2012", "/home/matlab_3/df_root/opendap/ACORN/gridded_1h-avg-current-map_non-QC/BONC/2012", 'nc')
-
-       f.close()
-    else:
-       print "Failed to connect to the datafabric so exiting."
-       sys.exit()
+    os.system("/usr/bin/rsync -aR --remove-source-files /home/ggalibert/DATA/ACORN/WERA/radial_nonQC/output/datafabric/gridded_1havg_currentmap_nonQC/./ /mnt/imos-t3/IMOS/opendap/ACORN/gridded_1h-avg-current-map_non-QC/")
+	os.system("/usr/bin/rsync -aR --remove-source-files /home/ggalibert/DATA/ACORN/CODAR/nonQC_gridded/output/datafabric/gridded_1havg_currentmap_nonQC/./ /mnt/imos-t3/IMOS/opendap/ACORN/gridded_1h-avg-current-map_non-QC/")
