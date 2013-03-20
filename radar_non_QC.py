6e940bb577eda62b59426b30e25904ef70cba10b
ggalibert <ggalibert@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2013-03-20 06:40:58 +0000

    git-svn-id: https://svn.emii.org.au/repos/acorn/trunk@8839 80f082c3-aa3d-4a8a-bc4c-16289f4b763f

--- radar_non_QC.py
+++ radar_non_QC.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
         sys.exit()
        
     os.system("/usr/bin/rsync -aR --remove-source-files /home/ggalibert/DATA/ACORN/WERA/radial_nonQC/output/datafabric/gridded_1havg_currentmap_nonQC/./ /mnt/imos-t3/IMOS/opendap/ACORN/gridded_1h-avg-current-map_non-QC/")
-	os.system("/usr/bin/rsync -aR --remove-source-files /home/ggalibert/DATA/ACORN/CODAR/nonQC_gridded/output/datafabric/gridded_1havg_currentmap_nonQC/./ /mnt/imos-t3/IMOS/opendap/ACORN/gridded_1h-avg-current-map_non-QC/")
+    os.system("/usr/bin/rsync -aR --remove-source-files /home/ggalibert/DATA/ACORN/CODAR/nonQC_gridded/output/datafabric/gridded_1havg_currentmap_nonQC/./ /mnt/imos-t3/IMOS/opendap/ACORN/gridded_1h-avg-current-map_non-QC/")
