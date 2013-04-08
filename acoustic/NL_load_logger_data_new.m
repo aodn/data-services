92579e6dfc2799ad9911bf484463efe4d04f388f
mhidas <mhidas@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2013-04-08 23:11:38 +0000

    Newer version from Sasha.
    git-svn-id: https://svn.emii.org.au/repos/anmn/trunk@8872 80f082c3-aa3d-4a8a-bc4c-16289f4b763f

--- acoustic/NL_load_logger_data_new.m
+++ acoustic/NL_load_logger_data_new.m
@@ -1,21 +1,32 @@
-function [Header, Volts, FSample, Rec_start_time] = NL_load_logger_data_new(FileName)
+function [Header, Volts, FSample, Rec_start_time, overload] = NL_load_logger_data_new(FileName, Status)
 
-% NL_load_logger_data.m reads noise logger raw (binary) data file 
+% NL_load_logger_data_new.m reads noise logger raw (binary) data files 
 % FileName is the file name
-% Use: [Header, Volts, FSample, Schedule] = NL_load_logger_data(FileName);
+% Use: [Header, Volts, FSample, Rec_start_time, overload] = NL_load_logger_data(FileName, Status);
 % Output:
-% Volts is a vector of the signal amplitude in volts at ADC;
-% FSample is the sampling frequency in Hz;
-% Header is a ASCII header of the binary data file;
-% Rec_start_time it the UTC time of the first sample in the recording in
-% hours re 00:00:00 01/01/0000;
+% 1) Header is an ASCII header of the binary data file;
+% 2) Volts is a vector of the signal amplitude in volts at ADC;
+% 3) FSample is the sampling frequency in Hz;
+% 4) Rec_start_time is the UTC time of the first sample in the recording in
+% days re 00:00:00 01/01/0000;
+% 5) overload(Channel No) = 1 if the recording channel was saturated (waveform
+% truncated), otherwise overload(Cannel No) = 0;
 
 % Modified version of Frank's routine to read binary data files recorded by
 % CMST sea noise loggers.
 % Calculation of the UTS time of the first sample, including fractions of a
 % second, is added;
-
 % A. Gavrilov, CMST, Curtin University, Nov-2012
+
+% Inpit Status is added. If it exists (any number or string), a warning
+% message about ADC overload will be displayed
+% A. Gavrilov, CMST, Curtin University, Jan-2013
+
+if nargin < 2
+    Status = 0;
+else
+    Status = 1;
+end
    
 NHeader = 5;  % Number of header lines
 NMarker = 6; % Number of marker lines
@@ -67,13 +78,21 @@ NumSamples = ceil(SLength*FSample);
 % read data
 Volts = fread(Fid, [NumActiveCh,NumSamples], 'uint16');	%not actually volts at this stage
 Volts = Volts';
-
+szVolts = size(Volts);
+overload = [0,0];
+for nch = 1:szVolts(2)
+    if max(Volts(:,nch)) > 6e4
+        overload(nch) = 1;
+        if Status
+            warndlg('Logger was overloaded - signal is truncated','modal'); 
+        end
+    end
+end
 %Convert to Volts
 %Modified Franks method
 Fullscale = 5;	%0 to 5 V
 %Multiply by this factor to convert A/D counts to volts 0-5
 CountsToVolts = Fullscale/65536; 
-szVolts = size(Volts);
 for i=1:szVolts(2);	%need to correct each column
 	Volts(:,i) = (CountsToVolts.*Volts(:,i))-mean(Volts(:,i).*CountsToVolts);
 end
@@ -86,8 +105,10 @@ end
 %read ASCII lines from Footer into Header
 fseek(Fid,-MarkerLength,'cof');    
 for ISub = 1:NMarker
-	tmp=fgetl(Fid);
-	Header = strvcat(Header,tmp);
+	tmp = fgetl(Fid);
+    if tmp ~= -1
+        Header = strvcat(Header,tmp);
+    end
 end
 
 % Calculate exact UTC time of the first sample in the recording based on Footer data:
