f59cfd9e1bfdc53c9ef188d680d48d3c8169d826
mhidas <mhidas@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2013-04-19 06:52:12 +0000

    read move_view in sqlite3 harvest database and write a script to move files to opendap
    git-svn-id: https://svn.emii.org.au/repos/anmn/trunk@8896 80f082c3-aa3d-4a8a-bc4c-16289f4b763f

---
+++ IMOSfile/writeMoveScript.py
@@ -0,0 +1,59 @@
+#! /usr/bin/env python
+#
+# Use info in the sqlite3 database set up by stagingHarvest.py to
+# generate a script that will move the files to the correct public
+# directories, archiving any previous versions present.
+
+import sys, time
+from sqlite3 import connect
+from os.path import join
+
+
+# connect to database
+dbFile = 'harvest.db'
+conn = connect(dbFile)
+curs = conn.cursor()
+
+# From move_view, select files which either don't have a version
+# already in public or where the version in public is an older version
+sql = """
+SELECT source_path, filename, dest_path, old_file, old_path FROM move_view 
+WHERE old_file IS NULL  OR creation_time > old_creation_time
+ORDER BY dest_path;
+"""
+curs.execute(sql)
+
+# commands & bits to use in script
+MV='mv -iv '
+MKDIR='mkdir -pv '
+dateDir = 'old_' + time.strftime('%Y%m%d')
+
+# keep track of directories made, so we don't try to make them over
+# and over again
+archiveDirs = set()
+publicDirs = set()
+prevDir = ''
+
+# for each file...
+for source_path, filename, dest_path, old_file, old_path in curs.fetchall():
+
+    if dest_path <> prevDir: 
+        print
+        print 'echo ........................................................'
+  
+    # if an older version of the file exists, move it to archive
+    if old_file:
+        archive_path = join(dest_path.replace('opendap','archive'), dateDir)
+        if archive_path not in archiveDirs:
+            print '\n', MKDIR, archive_path
+            archiveDirs.add(archive_path)
+        print MV, join(old_path, old_file), '  ', archive_path
+
+    # move new file to public
+    if dest_path not in publicDirs:
+        print MKDIR, dest_path
+        publicDirs.add(dest_path)
+    print MV,  join(source_path, filename),  dest_path
+
+    prevDir = dest_path
+
