82276c7b8cc8e3fab1badd48590e4a72425b9de0
ggalibert <ggalibert@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2013-04-26 04:27:04 +0000

    -rsync is not moving/deleting files that are being uploaded anymore.
    git-svn-id: https://svn.emii.org.au/repos/acorn/trunk@8914 80f082c3-aa3d-4a8a-bc4c-16289f4b763f

--- BASH/move_FV00_radial.sh
+++ BASH/move_FV00_radial.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+# rsync ACORN radial FV00 data from STAGING to OPENDAP
 
 # Need to delete empty files and directories not to move them
 /usr/bin/find /mnt/imos-t4/IMOS/staging/ACORN/radial/ -type f -empty -delete
 /usr/bin/find /mnt/imos-t4/IMOS/staging/ACORN/radial/ -type d -empty -delete
 
-# rsync ACORN radial FV00 data from STAGING to OPENDAP
-/usr/bin/rsync -aR --remove-source-files --include '+ */' --include '*FV00*.nc'  --exclude '- *' /mnt/imos-t4/IMOS/staging/ACORN/radial/./ /mnt/imos-t3/IMOS/opendap/ACORN/radial/
+# we need to prevent from copying growing files
+# (files still being uploaded and not finished at the time we launch rsync)
+# so we look for files last accessed for greater than 1min ago
+/usr/bin/find /mnt/imos-t4/IMOS/staging/ACORN/radial/ -type f -amin +1 -name "*FV00_radial.nc" -printf %P\\0 | rsync -a --remove-source-files --files-from=- --from0 /mnt/imos-t4/IMOS/staging/ACORN/radial/ /mnt/imos-t3/IMOS/opendap/ACORN/radial/
--- BASH/move_FV00_vector.sh
+++ BASH/move_FV00_vector.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+# rsync ACORN vector FV00 data from STAGING to OPENDAP
 
 # Need to rename files with shorter time_stamps
+# not anymore : Arnstein should have fixed this problem.
 #/usr/bin/find /mnt/imos-t4/IMOS/staging/ACORN/sea-state/ -type f | awk 'BEGIN {FS="_"} {if (length($4) == 14) print("mv "$0" "$1"_"$2"_"$3"_"substr($4,1,13)"00Z_"$5"_"$6"_"$7);}' | bash
 
 # Need to delete empty files and directories not to move them
 /usr/bin/find /mnt/imos-t4/IMOS/staging/ACORN/sea-state/ -type f -empty -delete
 /usr/bin/find /mnt/imos-t4/IMOS/staging/ACORN/sea-state/ -type d -empty -delete
 
-# rsync ACORN vector FV00 data from STAGING to OPENDAP
-/usr/bin/rsync -aR --remove-source-files --include '+ */' --include '*FV00*.nc'  --exclude '- *' /mnt/imos-t4/IMOS/staging/ACORN/sea-state/./ /mnt/imos-t3/IMOS/opendap/ACORN/vector/
+# we need to prevent from copying growing files
+# (files still being uploaded and not finished at the time we launch rsync)
+# so we look for files last accessed for greater than 1min ago
+/usr/bin/find /mnt/imos-t4/IMOS/staging/ACORN/sea-state/ -type f -amin +1 -name "*FV00_sea-state.nc" -printf %P\\0 | rsync -a --remove-source-files --files-from=- --from0 /mnt/imos-t4/IMOS/staging/ACORN/sea-state/ /mnt/imos-t3/IMOS/opendap/ACORN/vector/
