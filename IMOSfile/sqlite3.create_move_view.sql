032a6cda9fd119f598e6112239aaf9ccebf88295
mhidas <mhidas@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2013-04-30 03:32:37 +0000

    separate scripts to create tables and views
    git-svn-id: https://svn.emii.org.au/repos/anmn/trunk@8927 80f082c3-aa3d-4a8a-bc4c-16289f4b763f

---
+++ IMOSfile/sqlite3.create_move_view.sql
@@ -0,0 +1,36 @@
+
+DROP VIEW IF EXISTS good_to_go;
+
+CREATE VIEW good_to_go AS
+  SELECT source_path,
+  	 filename,
+	 dest_path,
+	 creation_time,
+	 sub_facility,
+	 product_code,
+	 file_version,
+	 n_files,
+	 nullif(first_created, last_created) AS first_created
+  FROM (SELECT 
+          product_code,
+          file_version,
+          count(*) AS n_files,
+	  min(creation_time) AS first_created,
+          max(creation_time) AS last_created
+        FROM staging
+        WHERE filename_errors IS NULL  AND  extension = 'nc'
+        GROUP BY product_code, file_version
+       ) AS grouped 
+       JOIN staging 
+       USING (product_code, file_version)
+  WHERE creation_time == last_created;
+
+
+DROP VIEW IF EXISTS move_view;
+
+CREATE VIEW move_view AS
+  SELECT good_to_go.*,
+         opendap.filename AS old_file,
+         opendap.source_path AS old_path,
+         opendap.creation_time AS old_creation_time
+  FROM good_to_go LEFT JOIN opendap USING (product_code, file_version);
--- IMOSfile/sqlite3.create_tables.sql
+++ IMOSfile/sqlite3.create_tables.sql
@@ -49,20 +49,3 @@ CREATE TABLE opendap
   end_time timestamp with time zone,
   creation_time timestamp with time zone
 );
-
-
-DROP VIEW IF EXISTS move_view;
-
-CREATE VIEW move_view AS
-  SELECT staging.source_path,
-         staging.filename,
-         staging.dest_path,
-         staging.creation_time,
-	 staging.sub_facility,
-	 staging.product_code,
-	 staging.file_version,
-         opendap.filename AS old_file,
-         opendap.source_path AS old_path,
-         opendap.creation_time AS old_creation_time
-  FROM staging LEFT JOIN opendap USING (product_code, file_version)
-  WHERE staging.filename_errors IS NULL  AND  staging.extension = 'nc';
