0435ad6a7f4febef721c5fd79ffcdca71eac53de
ggalibert <ggalibert@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2013-05-08 02:26:31 +0000

    -replace python scripts to call matlab by shell scripts
    git-svn-id: https://svn.emii.org.au/repos/acorn/trunk@8961 80f082c3-aa3d-4a8a-bc4c-16289f4b763f

--- BASH/radar_non_QC.sh
+++ BASH/radar_non_QC.sh
@@ -6,7 +6,7 @@ source /home/ggalibert/STORAGE.env
 source /home/ggalibert/ACORN.env
 
 # Run Matlab script to produce FV00 hourly averaged gridded files
-matlab -nodisplay -r  \"cd(getenv('ACORN_EXP')); addpath(fullfile('.', 'Util')); acorn_summary('WERA', false); acorn_summary('CODAR', false); exit\"
+matlab -nodisplay -r  "cd(getenv('ACORN_EXP')); addpath(fullfile('.', 'Util')); acorn_summary('WERA', false); acorn_summary('CODAR', false); exit"
 
 # Move produced files to OPENDAP
 rsync -vaR --remove-source-files $DATA/ACORN/WERA/radial_nonQC/output/datafabric/gridded_1havg_currentmap_nonQC/./ $OPENDAP/ACORN/gridded_1h-avg-current-map_non-QC/
