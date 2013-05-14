f9c6555c318670853314fdff4a7052147b53a722
ggalibert <ggalibert@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2013-05-14 06:04:11 +0000

    git-svn-id: https://svn.emii.org.au/repos/acorn/trunk@8973 80f082c3-aa3d-4a8a-bc4c-16289f4b763f

--- BASH/radar_QC.sh
+++ BASH/radar_QC.sh
@@ -6,7 +6,7 @@ source /home/ggalibert/STORAGE.env
 source /home/ggalibert/ACORN.env
 
 # Run Matlab script to produce FV01 hourly averaged gridded files
-matlab -nodisplay -r  "cd(getenv('ACORN_EXP')); addpath(fullfile('.', 'Util')); acorn_summary('WERA', true); exit"
+matlab -nodisplay -r "cd(getenv('ACORN_EXP')); addpath(fullfile('.', 'Util')); acorn_summary('WERA', true); exit"
 
 # Move produced files to OPENDAP
 rsync -vaR --remove-source-files $DATA/ACORN/WERA/radial_QC/output/datafabric/gridded_1havg_currentmap_QC/./ $OPENDAP/ACORN/gridded_1h-avg-current-map_QC/
