e23e68620af185b1979484985ecd94244a53d61d
ggalibert <ggalibert@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2013-05-23 07:54:20 +0000

    -set deflation level to 6
    git-svn-id: https://svn.emii.org.au/repos/acorn/trunk@9001 80f082c3-aa3d-4a8a-bc4c-16289f4b763f

--- radar_WERA_create_current_data.m
+++ radar_WERA_create_current_data.m
@@ -539,10 +539,7 @@ end
 netcdfFilename = ['IMOS_ACORN_V_', dateforfileSQL, 'Z_', site_code, '_' fileVersionCode '_1-hour-avg.nc'];
 netcdfoutput = fullfile(finalPathOutput, netcdfFilename);
 
-for i=0:9
-		createNetCDF([netcdfoutput num2str(i)], site_code, isQC, timenc, timeStr, X, Y, Zrad, Urad, Vrad, QCrad, true, i);
-end
-%createNetCDF([netcdfoutput num2str(i)], site_code, isQC, timenc, timeStr, X, Y, Zrad, Urad, Vrad, QCrad, true, 5);
+createNetCDF(netcdfoutput, site_code, isQC, timenc, timeStr, X, Y, Zrad, Urad, Vrad, QCrad, true, 6);
 
 end
 
