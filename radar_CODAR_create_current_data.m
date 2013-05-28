02ebd0c03b36f3ba3184049c0c7f17c981053b45
ggalibert <ggalibert@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2013-05-28 01:01:21 +0000

    git-svn-id: https://svn.emii.org.au/repos/acorn/trunk@9010 80f082c3-aa3d-4a8a-bc4c-16289f4b763f

--- radar_CODAR_create_current_data.m
+++ radar_CODAR_create_current_data.m
@@ -158,6 +158,8 @@ end
 netcdfFilename = ['IMOS_ACORN_V_', dateforfileSQL, 'Z_', site_code, '_' fileVersionCode '_1-hour-avg.nc'];
 netcdfoutput = fullfile(finalPathOutput, netcdfFilename);
 
-createNetCDF(netcdfoutput, site_code, isQC, timenc, timeStr, X, Y, Zrad, Urad, Vrad, QCrad, true, 6, meta);
+for i=0:9
+createNetCDF([netcdfoutput num2str(i)], site_code, isQC, timenc, timeStr, X, Y, Zrad, Urad, Vrad, QCrad, true, i, meta);
+end
 
 end
