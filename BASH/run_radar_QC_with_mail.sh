0439ac2c48a785a3d4b432de09100892a41d0ee3
ggalibert <ggalibert@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2013-07-22 02:27:26 +0000

    git-svn-id: https://svn.emii.org.au/repos/acorn/trunk@9149 80f082c3-aa3d-4a8a-bc4c-16289f4b763f

--- BASH/run_radar_QC_with_mail.sh
+++ BASH/run_radar_QC_with_mail.sh
@@ -2,4 +2,4 @@
 
 ./radar_QC.sh &> ./radar_QC.log
 cat ./radar_QC.log | tr -d '\033[?1[hl]\003[=>]' | mailx -s '<ggalibert@imos-5> $ACORN_EXP/BASH/radar_QC.sh' -c sebastien.mancini@utas.edu.au guillaume.galibert@utas.edu.au
-#rm -f ./radar_QC.log
+rm -f ./radar_QC.log
