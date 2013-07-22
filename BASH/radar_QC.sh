9e12a72c44bd0f666239d99556e843d7f2aa7606
ggalibert <ggalibert@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2013-07-22 01:50:43 +0000

    git-svn-id: https://svn.emii.org.au/repos/acorn/trunk@9146 80f082c3-aa3d-4a8a-bc4c-16289f4b763f

--- BASH/radar_QC.sh
+++ BASH/radar_QC.sh
@@ -2,7 +2,7 @@
 
 date
 tic=$(date +%s.%N)
-echo ''
+printf "\n"
 
 # Need to set the environment variables relevant for ACORN
 source /home/ggalibert/DEFAULT_PATH.env
@@ -12,18 +12,18 @@ source /home/ggalibert/ACORN.env
 # Run Matlab script to produce FV01 hourly averaged gridded files
 matlab -nodisplay -r "cd(getenv('ACORN_EXP')); addpath(fullfile('.', 'Util')); acorn_summary('WERA', true); exit"
 
-echo ''
+printf "\n"
 date
 toc=$(date +%s.%N)
 printf "%6.1Fs\tFV01 radial files processed to hourly averaged gridded files\n"  $(echo "$toc - $tic"|bc )
 
 tic=$(date +%s.%N)
-echo ''
+printf "\n"
 
 # Move produced files to OPENDAP
 rsync -vaR --remove-source-files $DATA/ACORN/WERA/radial_QC/output/datafabric/gridded_1havg_currentmap_QC/./ $OPENDAP/ACORN/gridded_1h-avg-current-map_QC/
 
-echo ''
+printf "\n"
 date
 toc=$(date +%s.%N)
 printf "%6.1Fs\tFV01 hourly averaged gridded files moved from WIP to OPENDAP\n"  $(echo "$toc - $tic"|bc )
