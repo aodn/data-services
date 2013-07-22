f1797c4f4791b5e4a83a4080775d7d5cd70c151d
mhidas <mhidas@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2013-07-22 08:03:31 +0000

    copied from anmn/trunk/harvest @ r9117
    git-svn-id: https://svn.emii.org.au/repos/abos/trunk@9152 80f082c3-aa3d-4a8a-bc4c-16289f4b763f

---
+++ sqlite3.create_tables.sql
@@ -0,0 +1,51 @@
+
+DROP TABLE IF EXISTS staging;
+
+CREATE TABLE staging
+(  
+  source_path character varying,
+  filename character varying,
+  extension character varying,
+  dest_path character varying,
+  facility character varying,
+  sub_facility character varying,
+  data_code character varying,
+  data_category character varying,
+  site_code character varying,
+  platform_code character varying,
+  file_version character varying,
+  product_code character varying,
+  deployment_code character varying,
+  instrument character varying,
+  instrument_depth real,
+  filename_errors character varying,
+  start_time timestamp with time zone,
+  end_time timestamp with time zone,
+  creation_time timestamp with time zone
+);
+
+
+DROP TABLE IF EXISTS opendap;
+
+CREATE TABLE opendap
+(  
+  source_path character varying,
+  filename character varying,
+  extension character varying,
+  dest_path character varying,
+  facility character varying,
+  sub_facility character varying,
+  data_code character varying,
+  data_category character varying,
+  site_code character varying,
+  platform_code character varying,
+  file_version character varying,
+  product_code character varying,
+  deployment_code character varying,
+  instrument character varying,
+  instrument_depth real,
+  filename_errors character varying,
+  start_time timestamp with time zone,
+  end_time timestamp with time zone,
+  creation_time timestamp with time zone
+);
