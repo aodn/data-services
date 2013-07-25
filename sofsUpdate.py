c6ad0b539f310ce1d0e8abe92c5812b64354599e
mhidas <mhidas@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2013-07-25 02:55:14 +0000

    logging more info on what happened to each file
    git-svn-id: https://svn.emii.org.au/repos/abos/trunk@9168 80f082c3-aa3d-4a8a-bc4c-16289f4b763f

--- sofsUpdate.py
+++ sofsUpdate.py
@@ -63,13 +63,24 @@ def destPath(info, basePath=''):
 
 def updateFile(source, destDir, log=None, dry_run=False):
     """
-    Synchronise source (file) to destDir, copying the file only if it
+    Synchronise source file to destDir, copying the file only if it
     doesn't exist at destDir or if source has been modified more
-    rencently. If destDir is not an existing directory, create it.
+    rencently. If destDir is not an existing directory, create it.  If
+    log is set to an open file object, log the sync command to it.  If
+    dry_run is True, don't more anything, just log actions.
 
-    Return True if the file was updated at destDir.
-    If log is set to an open file object, log the sync command to it.
+    Return a single character indicating the result: 
+    'A' - added new file; 
+    'U' - updated existing file; 
+    'E' - existing file, no update;
+    'F' - update failed.
     """
+    # make sure source is a file
+    if not os.path.isfile(source):
+        print 'WARNING (updateFile): %s is not a file! Skipping.'
+        return 'F'
+
+    # make sure destDir exists
     if not os.path.isdir(destDir):
         try:
             if not dry_run: 
@@ -79,16 +90,33 @@ def updateFile(source, destDir, log=None, dry_run=False):
         except:
             if log: 
                 print >> log, 'failed to create directory', destDir
-            return False
+            return 'F'
 
+    # check if destination file exists (to distinguish between add or
+    # update)
+    sourceFile = os.path.basename(source)
+    destPath = os.path.join(destDir, sourceFile)
+    if os.path.isfile(destPath):
+        status = 'U'
+    else:
+        status = 'A'
+
+    # create, log and run the sync command
     syncCmd = 'rsync -uvt'
     if dry_run: syncCmd += 'n'
     cmd = ' '.join([syncCmd, source, destDir])
     if log: 
         print >> log, cmd
+    outPipe = os.popen(cmd)
+    result = outPipe.readlines()
+    if outPipe.close():  # rsync returned non-zero error status
+        return 'F'
 
-    result = os.popen(cmd).readlines()
-    return  result[0].strip() == os.path.basename(source)
+    # work out return value
+    if result[0].strip() != sourceFile:  # no update
+        status = 'E'
+
+    return status
 
 
 ### MAIN ###
@@ -110,6 +138,7 @@ sourceFiles = []
 updatedFiles = []
 existingFiles = []
 skippedFiles = []
+failedFiles = []
 LOG = open('sofsUpdate.log', 'w')
 
 print 'sorting files...'
@@ -126,30 +155,38 @@ for curDir, dirs, files in os.walk(args.tmp_dir):
         # if not netcdf file, skip with warning
         if info['extension'] != 'nc':
             print 'WARNING! Non-netCDF file:', fileName
-            skippedFiles.append(sourcePath)
+            skippedFiles.append('S '+sourcePath)
             continue
 
         # work out destination 
         destinationPath = destPath(info, basePath=args.target_dir)
         if not destinationPath:
             print 'WARNING! Unknown destination for', fileName
-            skippedFiles.append(sourcePath)
+            skippedFiles.append('S '+sourcePath)
             continue
 
-        # synch file to its destination (only copy if file is new)
-        if updateFile(sourcePath, destinationPath, log=LOG, dry_run=args.dry_run):
-            updatedFiles.append(sourcePath)
+        # synch file to its destination
+        
+        status = updateFile(sourcePath, destinationPath, 
+                            log=LOG, dry_run=args.dry_run)
+        logText = '%s %s' % (status, os.path.join(destinationPath, fileName))
+        if status == 'F':
+            failedFiles.append(logText)
+        elif status == 'E':
+            existingFiles.append(logText)
         else:
-            existingFiles.append(sourcePath)
+            updatedFiles.append(logText)
 
 
 print '%5d files processed' % len(sourceFiles)
 print '%5d files updated' % len(updatedFiles)
 print '%5d files already at destination' % len(existingFiles)
+print '%5d files failed to sync' % len(failedFiles)
 print '%5d files skipped' % len(skippedFiles)
 
 print >> LOG, '\nUpdated:\n' + '\n'.join(updatedFiles)
 print >> LOG, '\nExisting:\n' + '\n'.join(existingFiles)
+print >> LOG, '\nFailed:\n' + '\n'.join(failedFiles)
 print >> LOG, '\nSkipped:\n' + '\n'.join(skippedFiles)
 
 LOG.close()
