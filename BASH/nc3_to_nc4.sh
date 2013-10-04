c42eb53cc203bbe1378e15b567edbf956e32bb63
ggalibert <ggalibert@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2013-10-04 00:18:06 +0000

    git-svn-id: https://svn.emii.org.au/repos/acorn/trunk@9202 80f082c3-aa3d-4a8a-bc4c-16289f4b763f

--- BASH/nc3_to_nc4.sh
+++ BASH/nc3_to_nc4.sh
@@ -17,10 +17,10 @@ ncName=${ncPath##*/}
 # check for a global attribute netcdf_version being 3.6
 metaNcVer=`ncdump -h $ncName | grep -E -i 'netcdf_version = "3.6"'`
 if [ ! -z "$metaNcVer" ]; then # metaNcVer is not empty
-	# convert to NetCDF4 with nco's Lempel-Ziv lossless compression level set to 5 (between 0 and 9)
+	# convert to NetCDF4 with nco's Lempel-Ziv lossless compression level set to 6 (between 0 and 9)
 	# I don't want the history global attribute to be updated and I force overwriting the output file
 	# (note that output file == input file is possible in nco)
-	ncks -4 -L 5 -h -O $ncPath $ncPath
+	ncks -4 -L 6 -h -O $ncPath $ncPath
 
 	# retrieve current NetCDF API's version
 	ncVersion=`nc-config --version | awk '{print $2}'`
@@ -28,8 +28,12 @@ if [ ! -z "$metaNcVer" ]; then # metaNcVer is not empty
 	# update netcdf_version global attribute
 	# I don't want the history global attribute to be updated
 	ncatted -a netcdf_version,global,m,c,$ncVersion -h $ncPath
+	
+	toc=$(date +%s.%N)
+	
+	printf "%6.1Fs\t$ncName converted to NetCDF4\n"  $(echo "$toc - $tic"|bc )
+else
+	toc=$(date +%s.%N)
+	
+	printf "%6.1Fs\t$ncName is already NetCDF4\n"  $(echo "$toc - $tic"|bc )
 fi
-
-toc=$(date +%s.%N)
-
-printf "%6.1Fs\t$ncName converted to NetCDF4\n"  $(echo "$toc - $tic"|bc )
