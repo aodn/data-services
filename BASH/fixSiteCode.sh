35c9bc6e91986f31c08cdadf20ce3c9e493315bf
ggalibert <ggalibert@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2013-12-18 01:18:40 +0000

    git-svn-id: https://svn.emii.org.au/repos/acorn/trunk@9217 80f082c3-aa3d-4a8a-bc4c-16289f4b763f

---
+++ BASH/fixSiteCode.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# test the number of input arguments
+if [ $# -ne 2 ]
+then
+  echo "Usage: $0 nc_file.nc site_code"
+  exit
+fi
+
+ncPath=$1
+siteCode=$2
+
+# get the file name without path
+ncName=${ncPath##*/}
+
+# check for a global attribute site_code
+metaNc=`ncdump -h $ncPath | grep -E -i "site_code"`
+if [ ! -z "$metaNc" ]; then # metaNc is not empty
+	# check for its value being $siteCode
+	metaNc=`ncdump -h $ncPath | grep -E -i "site_code = \"$siteCode\""`
+	if [ -z "$metaNc" ]; then # metaNc is empty
+		# update site_code global attribute
+		# I don't want the history global attribute to be updated
+		ncatted -a site_code,global,m,c,"$siteCode" -h $ncPath
+		printf "$ncName fixed with an updated site_code = $siteCode\n"
+	fi
+else
+	# create site_code global attribute
+	ncatted -a site_code,global,c,c,"$siteCode" -h $ncPath
+	printf "$ncName fixed with a new site_code = $siteCode\n"
+fi
