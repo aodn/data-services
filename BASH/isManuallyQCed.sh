1029db677099439930aad037badd79b5596dd16b
ggalibert <ggalibert@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2014-05-06 04:31:04 +0000

    git-svn-id: https://svn.emii.org.au/repos/acorn/trunk@9270 80f082c3-aa3d-4a8a-bc4c-16289f4b763f

--- BASH/isManuallyQCed.sh
+++ BASH/isManuallyQCed.sh
@@ -13,7 +13,8 @@ ncPath=$1
 ncName=${ncPath##*/}
 
 # check for a global attribute with relevant string
-metaNcVer=`ncdump -h $ncPath | grep -E -i 'Author flagged '`
-if [ ! -z "$metaNcVer" ]; then # metaNcVer is not empty
-	printf "$ncPath is manualy QCed\n"
+metaNcVer=`ncdump -h $ncPath | grep -E -i ':toolbox_version'` | cut -f 2 -d '"'
+meta=`ncdump -h $ncPath | grep -E -i 'Author flagged '`
+if [ ! -z "$meta" ]; then # meta is not empty
+	printf "$ncPath is manually QCed with toolbox version $metaNcVer\n"
 fi
