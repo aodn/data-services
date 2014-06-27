3ea3403e3af970ebcfcffc3fa1126e0e3f9fde9e
ggalibert <ggalibert@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2014-06-27 06:11:35 +0000

    git-svn-id: https://svn.emii.org.au/repos/acorn/trunk@9308 80f082c3-aa3d-4a8a-bc4c-16289f4b763f

--- BASH/checkMoreRecentRadials.sh
+++ BASH/checkMoreRecentRadials.sh
@@ -68,9 +68,19 @@ do
 						# This is not as accurate as if we would check on hourly basis rather than daily...
 						if [ $radialDirLastDate -gt $vectorDirLastDate ]; then
 							echo "$vectorDir needs to be re-processed"
+							if [ ${#site} -eq 3 ]
+								radar_non_QC.sh "{'$site'}" "'$year$month$dayT003000'" "'$year$month$dayT233000'" "{}" "''" "''"
+							else
+								radar_non_QC.sh "{}" "''" "''" "{'$site'}" "'$year$month$dayT000000'" "'$year$month$dayT230000'"
+							fi
 						fi
 					else
 						echo "$vectorDir needs to be processed"
+						if [ ${#site} -eq 3 ]
+							radar_non_QC.sh "{'$site'}" "'$year$month$dayT003000'" "'$year$month$dayT233000'" "{}" "''" "''"
+						else
+							radar_non_QC.sh "{}" "''" "''" "{'$site'}" "'$year$month$dayT000000'" "'$year$month$dayT230000'"
+						fi
 					fi
 				fi
 			done
