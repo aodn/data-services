05e4667bb77334526b58a0d54a675b2c000160aa
Dan Fruehauf <malkodan@gmail.com>
2014-07-04 12:11:32 +1000

    argo lftp job managed

---
+++ ARGO/lftp_sync.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ARGO_SRC_HOST=ftp.ifremer.fr
+ARGO_SRC_DIR=/ifremer/argo/dac
+ARGO_DEST=/mnt/opendap/1/IMOS/opendap/Argo/dac
+ARGO_LOG=/mnt/imos-t4/log/argo_lftp.log
+
+lftp -e "mirror -e --parallel=10 --log=$ARGO_LOG $ARGO_SRC $ARGO_DEST ; quit" $ARGO_SRC_HOST
