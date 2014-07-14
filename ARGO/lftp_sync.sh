cd67933ec455e86cba569ade4d5af5959591a823
Dan Fruehauf <malkodan@gmail.com>
2014-07-14 10:43:26 +1000

    hotfix argo lftp script

--- ARGO/lftp_sync.sh
+++ ARGO/lftp_sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ARGO_SRC_HOST=ftp.ifremer.fr
-ARGO_SRC_DIR=/ifremer/argo/dac
+ARGO_SRC=/ifremer/argo/dac
 ARGO_DEST=/mnt/opendap/1/IMOS/opendap/Argo/dac
 ARGO_LOG=/mnt/imos-t4/log/argo_lftp.log
 
