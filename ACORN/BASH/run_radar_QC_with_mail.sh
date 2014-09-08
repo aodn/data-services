b3385c3337890674b43ba30870e0886733d25065
Guillaume Galibert <guillaume.galibert@utas.edu.au>
2014-09-08 12:01:13 +1000

    Can now run several acorn jobs at the same time

--- ACORN/BASH/run_radar_QC_with_mail.sh
+++ ACORN/BASH/run_radar_QC_with_mail.sh
@@ -7,6 +7,6 @@ then
 	exit
 fi
 
-./radar_QC.sh "$1" "$2" "$3" &> ./radar_QC.log
-cat ./radar_QC.log | mailx -s '<ggalibert@imos-5> $ACORN_EXP/BASH/radar_QC.sh' -c sebastien.mancini@utas.edu.au guillaume.galibert@utas.edu.au
-rm -f ./radar_QC.log
+./radar_QC.sh "$1" "$2" "$3" &> ./radar_QC-$$.log
+cat ./radar_QC-$$.log | mailx -s '<ggalibert@imos-5> $ACORN_EXP/BASH/radar_QC.sh' -c sebastien.mancini@utas.edu.au guillaume.galibert@utas.edu.au
+rm -f ./radar_QC-$$.log
