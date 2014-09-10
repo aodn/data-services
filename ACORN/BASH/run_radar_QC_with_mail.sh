a5b369befc07c0a38b6b92b54c0c841fdae1fa93
Guillaume Galibert <guillaume.galibert@utas.edu.au>
2014-09-10 17:39:22 +1000

    Feed ailx with empty string when no body needs to be documented

--- ACORN/BASH/run_radar_QC_with_mail.sh
+++ ACORN/BASH/run_radar_QC_with_mail.sh
@@ -11,7 +11,19 @@ fi
 source /home/ggalibert/DEFAULT_PATH.env
 source /home/ggalibert/STORAGE.env
 
+# run the job
 logFile=$DATA/ACORN/radar_QC-$$.log
 ./radar_QC.sh "$1" "$2" "$3" &> $logFile
-cat $logFile | mailx -s '<ggalibert@imos-5> $ACORN_EXP/BASH/radar_QC.sh' -c sebastien.mancini@utas.edu.au guillaume.galibert@utas.edu.au
-rm -f $logFile
+
+# check the log file size before sending the mail
+maxFileSize=8000000
+actualFileSize=$(wc -c "$logFile" | cut -d ' ' -f 1)
+if [ $actualFileSize -ge $maxFileSize ]; then
+	gzip $logFile
+	echo "" | mailx -s '<ggalibert@imos-5> $ACORN_EXP/BASH/radar_QC.sh' -c sebastien.mancini@utas.edu.au guillaume.galibert@utas.edu.au -a "$logFile".gz
+else
+	cat $logFile | mailx -s '<ggalibert@imos-5> $ACORN_EXP/BASH/radar_QC.sh' -c sebastien.mancini@utas.edu.au guillaume.galibert@utas.edu.au
+fi
+
+# if mail is successful we can delete the logfile
+rm -f $logFile "$logFile".gz
