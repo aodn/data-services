084c1b24504c0011f7afb418000920763b443301
Guillaume Galibert <guillaume.galibert@utas.edu.au>
2014-09-30 17:48:55 +1000

    Switch staging source from ACORN to ACORN/acorn-migration-hierarchy for FV00 files.

--- ACORN/BASH/move_FV00_radial.sh
+++ ACORN/BASH/move_FV00_radial.sh
@@ -13,17 +13,17 @@ source /home/ggalibert/ACORN.env
 # we need to prevent from copying growing files
 # (files still being uploaded and not finished at the time we launch find)
 # so we look for files last accessed for greater than 5min ago
-find $STAGING/ACORN/radial/ -type f -amin +5 -name "*FV00_radial.nc" -printf %P\\n | sort > /tmp/move_FV00_radial.$$.list
+find $STAGING/ACORN/acorn-migration-hierarchy/radial/ -type f -amin +5 -name "*FV00_radial.nc" -printf %P\\n | sort > /tmp/move_FV00_radial.$$.list
 
 # we check these NetCDF files are not corrupted (basic check with ncdump not throwing any error, this will also spot empty files)
 touch /tmp/isCorruptedNC.$$.list
-cat /tmp/move_FV00_radial.$$.list | xargs -I {} isCorruptedNC.sh $STAGING/ACORN/radial/{} /tmp/isCorruptedNC.$$.list
+cat /tmp/move_FV00_radial.$$.list | xargs -I {} isCorruptedNC.sh $STAGING/ACORN/acorn-migration-hierarchy/radial/{} /tmp/isCorruptedNC.$$.list
 grep -v -f /tmp/isCorruptedNC.$$.list /tmp/move_FV00_radial.$$.list > /tmp/move_FV00_radial.$$.checkedList
 rm -f /tmp/isCorruptedNC.$$.list
 rm -f /tmp/move_FV00_radial.$$.list
 
 # we can finally move the remaining files
-cat /tmp/move_FV00_radial.$$.checkedList | rsync -va --remove-source-files --files-from=- $STAGING/ACORN/radial/ $OPENDAP/ACORN/radial/
+cat /tmp/move_FV00_radial.$$.checkedList | rsync -va --remove-source-files --files-from=- $STAGING/ACORN/acorn-migration-hierarchy/radial/ $OPENDAP/ACORN/radial/
 rm -f /tmp/move_FV00_radial.$$.checkedList
 
 printf "\n"
