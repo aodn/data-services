f4777437549a5cda61b711cdd32970e9077ec92f
bpasquer <benedicte.pasquer@utas.edu.au>
2014-10-28 14:23:56 +1100

    changed destination path : imos-t3 to opendap

--- ANFOG/BASH/moveANFOG_REALTIME.sh
+++ ANFOG/BASH/moveANFOG_REALTIME.sh
@@ -10,7 +10,7 @@
 
 rsync -aR  -O --remove-source-files  --include '+ */' --include '*.png' --include '*.txt' --exclude '- *' /mnt/imos-t4/IMOS/staging/ANFOG/realtime/slocum_glider/./ /mnt/imos-t4/IMOS/public/ANFOG/Realtime/slocum_glider
 rsync -aR -O --remove-source-files  --include '+ */' --include '*.png' --exclude '- *' /mnt/imos-t4/IMOS/staging/ANFOG/realtime/seaglider/./ /mnt/imos-t4/IMOS/public/ANFOG/Realtime/seaglider
-rsync -avR -O --remove-source-files /mnt/imos-t4/IMOS/staging/ANFOG/realtime/./ /mnt/imos-t3/IMOS/opendap/ANFOG/REALTIME
+rsync -avR -O --remove-source-files /mnt/imos-t4/IMOS/staging/ANFOG/realtime/./ /mnt/opendap/1/IMOS/opendap/ANFOG/REALTIME
 
 #for directory in $input_directories; do
 #	if [ `number_of_files_in_directory` -eq 0 ]; then
