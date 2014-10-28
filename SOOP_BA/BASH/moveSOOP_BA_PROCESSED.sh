7a82bfd08005ca7b28ad8787b3ef35b286491925
bpasquer <benedicte.pasquer@utas.edu.au>
2014-10-28 13:33:47 +1100

    modified SOOP_BA bash

--- SOOP_BA/BASH/moveSOOP_BA_PROCESSED.sh
+++ SOOP_BA/BASH/moveSOOP_BA_PROCESSED.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # move png to public
-rsync -aR  --include '+ */' --include '*.png' --exclude '- *' /mnt/imos-t4/IMOS/staging/SOOP/BA/Processed_data/./ /mnt/imos-t4/IMOS/public/SOOP/BA/
+rsync -avR --remove-source-files --include '+ */' --include '*.png' --exclude '- *' /mnt/imos-t4/IMOS/staging/SOOP/BA/Processed_data/./ /mnt/imos-t4/IMOS/public/SOOP/BA/
 # move data to opendap 
-rsync -aR --include '+ */' --include '*.nc' --exclude '*.png'  /mnt/imos-t4/IMOS/staging/SOOP/BA/Processed_data/./ /mnt/opendap/1/IMOS/opendap/SOOP/SOOP-BA/
+rsync -avR --remove-source-files --include '+ */' --include '*.nc' --exclude '*.png'  /mnt/imos-t4/IMOS/staging/SOOP/BA/Processed_data/./ /mnt/opendap/1/IMOS/opendap/SOOP/SOOP-BA/
