34609984c40ae5c012277eaede74b0c3265358d9
Dan Fruehauf <malkodan@gmail.com>
2014-10-30 16:25:15 +1100

    ANFOG/SOOP_BA: white spaces tidy up

--- ANFOG/BASH/moveANFOG_PROCESSED.sh
+++ ANFOG/BASH/moveANFOG_PROCESSED.sh
@@ -1,34 +1,20 @@
 #!/bin/bash
-#
+
 RSYNC_SOURCE_PATH=/mnt/imos-t4/IMOS/staging/ANFOG/processed
-#    
 RSYNC_SOURCE_IMAGES_PATH=/mnt/imos-t4/IMOS/staging/ANFOG/jpeg
-#        
 RSYNC_SOURCE_RAW_PATH=/mnt/imos-t4/IMOS/staging/ANFOG/raw
-#             
 RSYNC_DEST_PUBLIC_PATH=/mnt/imos-t4/IMOS/public/ANFOG
-#            
 RSYNC_DEST_ARCHIVE_PATH=/mnt/imos-t4/IMOS/archive/ANFOG
-#          
-RSYNC_DEST_PATH=/mnt/opendap/1/IMOS/opendap/ANFOG   
-#      
-# rsync between staging and opendap : move data to opendap 
-rsync -avr -O --remove-source-files ${RSYNC_SOURCE_PATH}/seaglider/ ${RSYNC_DEST_PATH}/seaglider/;
-rsync -avr -O --remove-source-files ${RSYNC_SOURCE_PATH}/slocum_glider/ ${RSYNC_DEST_PATH}/slocum_glider/;
-#
+RSYNC_DEST_PATH=/mnt/opendap/1/IMOS/opendap/ANFOG
+
+# rsync between staging and opendap : move data to opendap
+rsync -avr -O --remove-source-files ${RSYNC_SOURCE_PATH}/seaglider/ ${RSYNC_DEST_PATH}/seaglider/
+rsync -avr -O --remove-source-files ${RSYNC_SOURCE_PATH}/slocum_glider/ ${RSYNC_DEST_PATH}/slocum_glider/
+
 # rsync between staging and public : move images and kml to public
-rsync -ar -O --remove-source-files ${RSYNC_SOURCE_IMAGES_PATH}/seaglider/ ${RSYNC_DEST_PUBLIC_PATH}/seaglider/;
-rsync -ar -O --remove-source-files ${RSYNC_SOURCE_IMAGES_PATH}/slocum_glider/ ${RSYNC_DEST_PUBLIC_PATH}/slocum_glider/;
-#
+rsync -ar -O --remove-source-files ${RSYNC_SOURCE_IMAGES_PATH}/seaglider/ ${RSYNC_DEST_PUBLIC_PATH}/seaglider/
+rsync -ar -O --remove-source-files ${RSYNC_SOURCE_IMAGES_PATH}/slocum_glider/ ${RSYNC_DEST_PUBLIC_PATH}/slocum_glider/
+
 # rsync between staging and archive : move raw to archive
-rsync -ar -O --remove-source-files ${RSYNC_SOURCE_RAW_PATH}/seaglider/ ${RSYNC_DEST_ARCHIVE_PATH}/seaglider/;
-rsync -ar -O --remove-source-files ${RSYNC_SOURCE_RAW_PATH}/slocum_glider/ ${RSYNC_DEST_ARCHIVE_PATH}/slocum_glider/;  
-
-
-
-
-
-
-
-
-
+rsync -ar -O --remove-source-files ${RSYNC_SOURCE_RAW_PATH}/seaglider/ ${RSYNC_DEST_ARCHIVE_PATH}/seaglider/
+rsync -ar -O --remove-source-files ${RSYNC_SOURCE_RAW_PATH}/slocum_glider/ ${RSYNC_DEST_ARCHIVE_PATH}/slocum_glider/
--- SOOP_BA/BASH/moveSOOP_BA.sh
+++ SOOP_BA/BASH/moveSOOP_BA.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-#
+
 RSYNC_SOURCE_PATH=/mnt/imos-t4/IMOS/staging/SOOP/BA/Processed_data
-# 
 RSYNC_SOURCE_RAW_PATH=/mnt/imos-t4/IMOS/staging/SOOP/BA/Raw_data
-#
 RSYNC_DEST_PUBLIC_PATH=/mnt/imos-t4/IMOS/public/SOOP/BA
-#   
 RSYNC_DEST_RAW_PATH=/mnt/imos-t4/IMOS/archive/SOOP/BA/raw
-#
-RSYNC_DESTINATION_PATH=/mnt/opendap/1/IMOS/opendap/SOOP/SOOP-BA  
-#
+RSYNC_DESTINATION_PATH=/mnt/opendap/1/IMOS/opendap/SOOP/SOOP-BA
+
 # rsync between staging and public : move png's
-rsync -avr  --remove-source-files --include '+ */' --include '*.png' --exclude '- *' ${RSYNC_SOURCE_PATH}/ ${RSYNC_DEST_PUBLIC_PATH}/;
-#
+rsync -avr  --remove-source-files --include '+ */' --include '*.png' --exclude '- *' ${RSYNC_SOURCE_PATH}/ ${RSYNC_DEST_PUBLIC_PATH}/
+
 # rsync between staging and  archive: move raw data
-rsync -avr --remove-source-files ${RSYNC_SOURCE_RAW_PATH}/ ${RSYNC_DEST_RAW_PATH}/;
-#
-# rsync between staging and opendap : move data to opendap 
-rsync -avr --remove-source-files --include '+ */' --include '*.nc' --exclude '*.png'  ${RSYNC_SOURCE_PATH}/ ${RSYNC_DESTINATION_PATH}/;
+rsync -avr --remove-source-files ${RSYNC_SOURCE_RAW_PATH}/ ${RSYNC_DEST_RAW_PATH}/
+
+# rsync between staging and opendap : move data to opendap
+rsync -avr --remove-source-files --include '+ */' --include '*.nc' --exclude '*.png'  ${RSYNC_SOURCE_PATH}/ ${RSYNC_DESTINATION_PATH}/
