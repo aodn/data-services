8133f97adbcefb5d350978ce81ca9b1238cb5141
Dan Fruehauf <malkodan@gmail.com>
2014-11-06 17:31:47 +1100

    argo lftp uses environment variables

--- ARGO/lftp_sync.sh
+++ ARGO/lftp_sync.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# Avoid running this script if variables are undefined
+set -u
+
 ARGO_SRC_HOST=ftp.ifremer.fr
 ARGO_SRC=/ifremer/argo/dac
-ARGO_DEST=/mnt/opendap/1/IMOS/opendap/Argo/dac
-ARGO_LOG=/mnt/imos-t4/log/argo_lftp.log
+ARGO_DEST=$OPENDAP_DIR/1/IMOS/opendap/Argo/dac
+ARGO_LOG=$LOG_DIR/argo_lftp.log
 
 lftp -e "mirror -e --parallel=10 --log=$ARGO_LOG $ARGO_SRC $ARGO_DEST ; quit" $ARGO_SRC_HOST
