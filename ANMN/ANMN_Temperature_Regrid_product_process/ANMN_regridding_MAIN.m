59687d4dd0cb6c148a2c6779245f97af4f2dd7ed
bpasquer <benedicte.pasquer@utas.edu.au>
2014-11-18 10:06:14 +1100

    remove quality_control_log from glob attribute list; changed date format in attributes; check if directory exists for output

--- ANMN/ANMN_Temperature_Regrid_product_process/ANMN_regridding_MAIN.m
+++ ANMN/ANMN_Temperature_Regrid_product_process/ANMN_regridding_MAIN.m
@@ -5,13 +5,13 @@
 % regridding product .
 
 % READ PATH FROM CONFIG FILE
-Path2Opendap = readConfig('opendapdir');
-Path2Product = readConfig('productdir');
-Path2Wip = readConfig('wipdir');
-OutputDir= readConfig('outputdir');
-logfile = readConfig('log_file');
-failedlog = readConfig('failed_log');
-refdate = readConfig('reference_date');
+Path2Opendap = readConfig('opendapdir','configtst.txt');
+Path2Product = readConfig('productdir','configtst.txt');
+Path2Wip = readConfig('wipdir','configtst.txt');
+OutputDir= readConfig('outputdir','configtst.txt');
+logfile = readConfig('log_file','configtst.txt');
+failedlog = readConfig('failed_log','configtst.txt');
+refdate = readConfig('reference_date','configtst.txt');
 
 if ~isempty(refdate)
     fListIn  = ListTargetFiles(Path2Opendap,refdate);
@@ -21,18 +21,6 @@ else
     if ~isempty(fListIn)
 
         for i = 1:length(fListIn) 
-        % % % % % %     
-        % % % % % % %% Check if product for the listed deployments are already present on
-        % % % % % % % opendap . If so, move old product to archive.
-        % % % % % % path2fich = [Path2Opendap,fList(i).node,fList(i).site,'Temperature/gridded'];
-        % % % % % % 
-        % % % % % % if exist(path2fich,'dir')
-        % % % % % %     TGridFileList = dir(path2fich);
-        % % % % % %     comparer les id des deploiement present avec les nouveaux
-        % % % % % %     ceux deja present dpoivent etre deplace sur archive. 
-        % % % % % %     ATTETION : il serait plus sur de faire l'archivage apres avoir generer le nouveau produit
-        % % % % % %     
-
         %% PROCESS LISTED DEPLOYMENTS
              try
         % GENERATE THE REGRIDDED PRODUCT
@@ -44,20 +32,24 @@ else
         Tcreat = local_time_to_utc(now,30);
 
         % OUTPUT FILE NAME
+        % CREATE OUTPUT DIR IF NOT EXISTING ALREADY
+        if ~exist(fullfile(Path2Wip,OutputDir,fListIn(i).node,fListIn(i).site),'dir');
+            mkdir(fullfile(Path2Wip,OutputDir,fListIn(i).node,fListIn(i).site));
+        end
         fileOut = fullfile(Path2Wip,OutputDir,fListIn(i).node,fListIn(i).site,['IMOS_ANMN-',fListIn(i).node,'_','Temperature','_',Tstart,'_',fListIn(i).site,'_FV02_',fListIn(i).id,'-regridded_END-',Tend,'_C-',Tcreat,'.nc']);
 
         % CREATE A NETCDF FILE. 
         create_netcdf_deploy_v4nc(fListIn(i),fileOut,'TEMP',IallP,Tstamp,Zgrid,Lat,Lon,freq,nValStep)
 
          % RECORD PROCESSED DEPLOYMENT
-                  recorddate = datevec(now);
+                  recorddate = datevec(today);
                   fid =fopen(fullfile(Path2Wip,logfile),'a');
                   fprintf(fid,'%s \t Successfully processed : %s \t %s \n',recorddate,fListIn(i).node,fListIn(i).id);
                   fclose(fid);
 
              catch exception
         % RECORD FAILED DEPLOYMENTS  
-                   recorddate = datevec(now);
+                   recorddate = datevec(today);
                    fid =fopen(fullfile(Path2Wip,failedlog),'a');
                    fprintf(fid,'%s \t %s \t %s \t %s \t %s\n' ,recorddate,fListIn(i).id,exception.message,exception.stack(1).name,num2str(exception.stack(1).line));
                    fclose(fid);
@@ -69,4 +61,4 @@ else
       
     end
 end
-exit
+%exit
