b94ce6eab653631e2f9eb17e40c0a39f3dc71abf
bpasquer <benedicte.pasquer@utas.edu.au>
2014-11-25 11:47:24 +1100

    fix for running script  with specified reference date

--- ANMN/ANMN_Temperature_Regrid_product_process/ANMN_regridding_MAIN.m
+++ ANMN/ANMN_Temperature_Regrid_product_process/ANMN_regridding_MAIN.m
@@ -19,50 +19,49 @@ if ~isempty(refdate)
     fListIn  = ListTargetFiles(Path2Opendap,refdate);
 else
     fListIn  = ListTargetFiles(Path2Opendap);
+end
+if ~isempty(fListIn)
 
-    if ~isempty(fListIn)
+    for i = 1:length(fListIn) 
+    %% PROCESS LISTED DEPLOYMENTS
+         try
+    % GENERATE THE REGRIDDED PRODUCT
+    [Tstamp,Zgrid,IallP,Lat,Lon,freq,nValStep] = agregANMN_v_ave_RegularGrid(fListIn(i).path2file,fListIn(i).flistDeploy,'TEMP');
 
-        for i = 1:length(fListIn) 
-        %% PROCESS LISTED DEPLOYMENTS
-             try
-        % GENERATE THE REGRIDDED PRODUCT
-        [Tstamp,Zgrid,IallP,Lat,Lon,freq,nValStep] = agregANMN_v_ave_RegularGrid(fListIn(i).path2file,fListIn(i).flistDeploy,'TEMP');
+    % TIME STRING FOR OUTPOUT FILE NAME
+    Tstart = datestr(Tstamp(1),'yyyymmddTHHMMSSZ');
+    Tend = datestr(Tstamp(end),'yyyymmddTHHMMSSZ');
+    Tcreat = local_time_to_utc(now,30);
 
-        % TIME STRING FOR OUTPOUT FILE NAME
-        Tstart = datestr(Tstamp(1),'yyyymmddTHHMMSSZ');
-        Tend = datestr(Tstamp(end),'yyyymmddTHHMMSSZ');
-        Tcreat = local_time_to_utc(now,30);
-
-        % OUTPUT FILE NAME
-        % CREATE OUTPUT DIR IF NOT EXISTING ALREADY
-        if ~exist(fullfile(Path2Wip,OutputDir,fListIn(i).node,fListIn(i).site),'dir');
-            mkdir(fullfile(Path2Wip,OutputDir,fListIn(i).node,fListIn(i).site));
-        end
-        fileOut = fullfile(Path2Wip,OutputDir,fListIn(i).node,fListIn(i).site,['IMOS_ANMN-',fListIn(i).node,'_','Temperature','_',Tstart,'_',fListIn(i).site,'_FV02_',fListIn(i).id,'-regridded_END-',Tend,'_C-',Tcreat,'.nc']);
-
-        % CREATE A NETCDF FILE. 
-        create_netcdf_deploy_v4nc(fListIn(i),fileOut,'TEMP',IallP,Tstamp,Zgrid,Lat,Lon,freq,nValStep)
-
-         % RECORD PROCESSED DEPLOYMENT
-                  recorddate = datestr(now);
-                  disp('Process sucessful. Check log for new or udpdated product listing');
-                  fid =fopen(fullfile(Path2Wip,logfile),'a');
-                  fprintf(fid,'%s \t Successfully processed : %s \t %s \t %s \n',recorddate,fListIn(i).node,fListIn(i).site,fListIn(i).deploymt);
-                  fclose(fid);
-
-             catch exception
-        % RECORD FAILED DEPLOYMENTS  
-                   recorddate = datestr(now);
-                   disp('errors in deployment processing : check log for details');
-                   fid =fopen(fullfile(Path2Wip,failedlog),'a');
-                   fprintf(fid,'%s \t %s \t %s \t %s \t %s\n' ,recorddate,fListIn(i).id,exception.message,exception.stack(1).name,num2str(exception.stack(1).line));
-                   fclose(fid);
-                    continue
-             end
-        end
-    else
-        disp('No new deployment') ;
-      
+    % OUTPUT FILE NAME
+    % CREATE OUTPUT DIR IF NOT EXISTING ALREADY
+    if ~exist(fullfile(Path2Wip,OutputDir,fListIn(i).node,fListIn(i).site),'dir');
+        mkdir(fullfile(Path2Wip,OutputDir,fListIn(i).node,fListIn(i).site));
     end
+    fileOut = fullfile(Path2Wip,OutputDir,fListIn(i).node,fListIn(i).site,['IMOS_ANMN-',fListIn(i).node,'_','Temperature','_',Tstart,'_',fListIn(i).site,'_FV02_',fListIn(i).id,'-regridded_END-',Tend,'_C-',Tcreat,'.nc']);
+
+    % CREATE A NETCDF FILE. 
+    create_netcdf_deploy_v4nc(fListIn(i),fileOut,'TEMP',IallP,Tstamp,Zgrid,Lat,Lon,freq,nValStep)
+
+     % RECORD PROCESSED DEPLOYMENT
+              recorddate = datestr(now);
+              disp('Process sucessful. Check log for new or udpdated product listing');
+              fid =fopen(fullfile(Path2Wip,logfile),'a');
+              fprintf(fid,'%s \t Successfully processed : %s \t %s \t %s \n',recorddate,fListIn(i).node,fListIn(i).site,fListIn(i).deploymt);
+              fclose(fid);
+
+         catch exception
+    % RECORD FAILED DEPLOYMENTS  
+               recorddate = datestr(now);
+               disp('errors in deployment processing : check log for details');
+               fid =fopen(fullfile(Path2Wip,failedlog),'a');
+               fprintf(fid,'%s \t %s \t %s \t %s \t %s\n' ,recorddate,fListIn(i).id,exception.message,exception.stack(1).name,num2str(exception.stack(1).line));
+               fclose(fid);
+                continue
+         end
+    end
+else
+    disp('No new deployment') ;
+
 end
 exit
