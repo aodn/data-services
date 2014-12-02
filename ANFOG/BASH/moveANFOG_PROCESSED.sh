6f19c29f27fcd90ad29fbad8ad95b9f7f9fb5372
bpasquer <benedicte.pasquer@utas.edu.au>
2014-12-02 13:16:58 +1100

    apply filter : rsync only FV01 to opendap

--- ANFOG/BASH/moveANFOG_PROCESSED.sh
+++ ANFOG/BASH/moveANFOG_PROCESSED.sh
@@ -8,8 +8,8 @@ RSYNC_DEST_ARCHIVE_PATH=/mnt/imos-t4/IMOS/archive/ANFOG
 RSYNC_DEST_PATH=/mnt/opendap/1/IMOS/opendap/ANFOG
 
 # rsync between staging and opendap : move data to opendap
-rsync -vr -O --remove-source-files ${RSYNC_SOURCE_PATH}/seaglider/ ${RSYNC_DEST_PATH}/seaglider/
-rsync -vr -O --remove-source-files ${RSYNC_SOURCE_PATH}/slocum_glider/ ${RSYNC_DEST_PATH}/slocum_glider/
+rsync -vr -O --remove-source-files --include '+ */' --include '*FV01*.nc' --exclude '- *' ${RSYNC_SOURCE_PATH}/seaglider/ ${RSYNC_DEST_PATH}/seaglider/
+rsync -vr -O --remove-source-files --include '+ */' --include '*FV01*.nc' --exclude '- *' ${RSYNC_SOURCE_PATH}/slocum_glider/ ${RSYNC_DEST_PATH}/slocum_glider/
 
 # rsync between staging and public : move images and kml to public
 rsync -r -O --remove-source-files ${RSYNC_SOURCE_IMAGES_PATH}/seaglider/ ${RSYNC_DEST_PUBLIC_PATH}/seaglider/
