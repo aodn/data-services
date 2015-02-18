d575c8ecd36fef941e2cb0a44ff2a63b3de25417
lbesnard <laurent.besnard@utas.edu.au>
2015-02-18 18:24:39 +1100

    SRS SST NetCDF file checker
     * new script to check the consistency of the different gridded SRS
       products.
     * type gogoduck_file_checker -h if file in $PATH

---
+++ SRS/SRS_NetCDF_file_checker/tests.sh
@@ -0,0 +1,54 @@
+## L3S-1d/dn
+folderToCheck=/mnt/opendap/2/SRS/sst/ghrsst/L3S-1d/dn
+logDir=/home/lbesnard/gogoduck_checker_log/L3S-1d/dn
+gogoduck_file_checker -d $folderToCheck -l $logDir
+
+## L3S-1d/day
+folderToCheck=/mnt/opendap/2/SRS/sst/ghrsst/L3S-1d/day
+logDir=/home/lbesnard/gogoduck_checker_log/L3S-1d/day
+gogoduck_file_checker -d $folderToCheck -l $logDir
+
+## L3S-1d/night
+folderToCheck=/mnt/opendap/2/SRS/sst/ghrsst/L3S-1d/night
+logDir=/home/lbesnard/gogoduck_checker_log/L3S-1d/night
+gogoduck_file_checker -d $folderToCheck -l $logDir
+
+##############
+## L3S-3d/dn
+folderToCheck=/mnt/opendap/2/SRS/sst/ghrsst/L3S-3d/dn
+logDir=/home/lbesnard/gogoduck_checker_log/L3S-3d/dn
+gogoduck_file_checker -d $folderToCheck -l $logDir
+
+## L3S-3d/day
+folderToCheck=/mnt/opendap/2/SRS/sst/ghrsst/L3S-3d/day
+logDir=/home/lbesnard/gogoduck_checker_log/L3S-3d/day
+gogoduck_file_checker -d $folderToCheck -l $logDir
+
+## L3S-3d/night
+folderToCheck=/mnt/opendap/2/SRS/sst/ghrsst/L3S-3d/night
+logDir=/home/lbesnard/gogoduck_checker_log/L3S-3d/night
+gogoduck_file_checker -d $folderToCheck -l $logDir
+
+#################
+## L3C-1d/day
+folderToCheck=/mnt/opendap/2/SRS/sst/ghrsst/L3C-1d/day
+logDir=/home/lbesnard/gogoduck_checker_log/L3C-1d/day
+gogoduck_file_checker -d $folderToCheck -l $logDir
+
+## L3C-1d/night
+folderToCheck=/mnt/opendap/2/SRS/sst/ghrsst/L3C-1d/night
+logDir=/home/lbesnard/gogoduck_checker_log/L3C-1d/night
+gogoduck_file_checker -d $folderToCheck -l $logDir
+
+
+#################
+## L3C-3d/day
+folderToCheck=/mnt/opendap/2/SRS/sst/ghrsst/L3C-3d/day
+logDir=/home/lbesnard/gogoduck_checker_log/L3C-3d/day
+gogoduck_file_checker -d $folderToCheck -l $logDir
+
+## L3C-3d/night
+folderToCheck=/mnt/opendap/2/SRS/sst/ghrsst/L3C-3d/night
+logDir=/home/lbesnard/gogoduck_checker_log/L3C-3d/night
+gogoduck_file_checker -d $folderToCheck -l $logDir
+
