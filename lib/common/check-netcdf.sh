191b44bedfc3a9780274948fe946acc9a784d8e1
Dan Fruehauf <malkodan@gmail.com>
2015-03-27 12:31:50 +1100

    common bash functions
    used by event driven processing

---
+++ lib/common/check-netcdf.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+#############################
+# NETCDF CHECKING FUNCTIONS #
+#############################
+
+# TODO netcdf checker path here
+export NETCDF_CHECKER=/bin/true
+
+# a wrapper to run the netcdf checker
+# $1 - file
+# "$@" - netcdf check arguments
+_netcdf_checker() {
+    local file=$1; shift
+    local tmp_checker_output=`mktemp`
+    $NETCDF_CHECKER $file "$@" >& $tmp_checker_output
+    if [ $? -ne 0 ]; then
+        # log to specific log file and not the main log file
+        local log_file=`get_log_file $LOG_DIR $file`
+        cat $tmp_checker_output >> $log_file; rm -f $tmp_checker_output
+        log_error "NetCDF checker failed, verbose log saved at '$log_file'"
+        return 1
+    fi
+    rm -f $tmp_checker_output
+}
+export -f _netcdf_checker
+
+# checks a netcdf file
+# $1 - netcdf file to check
+check_netcdf() {
+    local file=$1; shift
+    ncdump -h $file >& /dev/null
+}
+export -f check_netcdf
+
+# checks a netcdf file for CF compliance
+# $1 - netcdf file to check
+check_netcdf_cf() {
+    local file=$1; shift
+    _netcdf_checker $file --test=cf
+}
+export -f check_netcdf_cf
+
+# checks a netcdf file for IMOS compliance
+# $1 - netcdf file to check
+check_netcdf_imos() {
+    local file=$1; shift
+    _netcdf_checker $file --test=imos
+}
+export -f check_netcdf_imos
+
+# checks a netcdf file for an IMOS facility compliance
+# $1 - netcdf file to check
+# $2 - facility specific plugin
+check_netcdf_facility() {
+    local file=$1; shift
+    local facility=$1; shift
+    _netcdf_checker $file --test=$facility
+}
+export -f check_netcdf_facility
+
---
+++ lib/common/log.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+###########
+# LOGGING #
+###########
+
+# returns path of log file for specific
+# $1 - base log directory (usually $LOG_FILE)
+# $2 - file being handled (sets context for logging)
+get_log_file() {
+    local log_dir=$1/$JOB_NAME; shift
+    local file=`basename $1`; shift
+    mkdir -p $log_dir
+    echo $log_dir/${file}.log
+}
+export -f get_log_file
+
+# log a message
+# $1 - log level
+# STDIN - log message
+_log_msg() {
+    local log_level=$1; shift
+
+    local msg
+    while read -e msg; do
+        logger -t $JOB_NAME -p ${SYSLOG_FACILITY}.${log_level} -- "$msg"
+    done
+}
+export -f _log_msg
+
+# log an error message
+# "$@" - log message
+log_error() {
+    echo "$@" | _log_msg "error"
+}
+export -f log_error
+
+# log a warning message
+# "$@" - log message
+log_warn() {
+    echo "$@" | _log_msg "warning"
+}
+export -f log_warn
+
+# log an information message
+# "$@" - log message
+log_info() {
+    echo "$@" | _log_msg "info"
+}
+export -f log_info
+
+
