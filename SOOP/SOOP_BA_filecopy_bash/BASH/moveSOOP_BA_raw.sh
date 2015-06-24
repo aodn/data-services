50ef04bb8db7a359246abc55677227b80903e461
bpasquer <benedicte.pasquer@utas.edu.au>
2015-06-24 14:31:23 +1000

    SOOP-BA : introduce environment variable to replace hardcoded path in shell scripts

--- SOOP/SOOP_BA_filecopy_bash/BASH/moveSOOP_BA_raw.sh
+++ SOOP/SOOP_BA_filecopy_bash/BASH/moveSOOP_BA_raw.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-RSYNC_SOURCE_RAW_PATH=/mnt/imos-t4/IMOS/staging/SOOP/BA/Raw_data
-RSYNC_DEST_RAW_PATH=/mnt/imos-t4/IMOS/archive/SOOP/BA/raw
+RSYNC_SOURCE_RAW_PATH=$INCOMING_DIR/SOOP/BA/Raw_data
+RSYNC_DEST_RAW_PATH=$ARCHIVE_DIR/SOOP/BA/raw
 
 # rsync between staging and  archive: move raw data
 rsync -vr --remove-source-files ${RSYNC_SOURCE_RAW_PATH}/ ${RSYNC_DEST_RAW_PATH}/
