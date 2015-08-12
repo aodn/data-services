017210595964e81f8d626331f45343575143ce8d
Dan Fruehauf <malkodan@gmail.com>
2015-08-12 11:49:38 +1000

    rsync argo exclude .snapshot directory
    ifremer introduced a .snapshot directory which we don't want to sync

--- ARGO/rsync.sh
+++ ARGO/rsync.sh
@@ -8,4 +8,4 @@ ARGO_SRC=argo
 ARGO_DEST=$OPENDAP_DIR/1/IMOS/opendap/Argo/dac
 ARGO_LOG=$LOG_DIR/argo_rsync.log
 
-rsync --times --delete -rzv $ARGO_SRC_HOST::$ARGO_SRC $ARGO_DEST &>> $LOG_DIR/argo_rsync.log
+rsync --exclude=.snapshot --times --delete -rzv $ARGO_SRC_HOST::$ARGO_SRC $ARGO_DEST &>> $LOG_DIR/argo_rsync.log
