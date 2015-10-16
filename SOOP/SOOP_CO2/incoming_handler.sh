e7c1c7daa128156a7c7e3158868a1d1cfe038fe4
Dan Fruehauf <malkodan@gmail.com>
2015-10-16 16:40:18 +1100

    SOOP_CO2: Fix usage of file_error

--- SOOP/SOOP_CO2/incoming_handler.sh
+++ SOOP/SOOP_CO2/incoming_handler.sh
@@ -21,10 +21,10 @@ main() {
 
     local tmp_dir=`mktemp -d`
     chmod a+rx $tmp_dir
-    unzip -q -u -o $file -d $tmp_dir || file_error "Error unzipping"
+    unzip -q -u -o $file -d $tmp_dir || file_error $file "Error unzipping"
 
     local nc_file
-    nc_file=`ls -1 $tmp_dir/*.nc | head -1` || file_error "Cannot find NetCDF file in zip bundle"
+    nc_file=`ls -1 $tmp_dir/*.nc | head -1` || file_error $file "Cannot find NetCDF file in zip bundle"
 
     check_netcdf      $nc_file || file_error_and_report_to_uploader $file $BACKUP_RECIPIENT "Not a valid NetCDF file"
 #    check_netcdf_cf   $file || file_error_and_report_to_uploader $file $BACKUP_RECIPIENT "File is not CF compliant"
@@ -32,7 +32,7 @@ main() {
 
     log_info "Processing '$nc_file'"
     local path
-    path=`$SCRIPTPATH/destPath.py $nc_file` || file_error "Cannot generate path for NetCDF file"
+    path=`$SCRIPTPATH/destPath.py $nc_file` || file_error $file "Cannot generate path for NetCDF file"
 
     # this file will need indexing, so use s3_put
     s3_put $nc_file IMOS/SOOP/$path/`basename $nc_file`
