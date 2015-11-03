2e2bc6e1165a6bdaa5f43307b795543c03562448
Dan Fruehauf <malkodan@gmail.com>
2015-11-03 16:57:21 +1100

    Improve lftp commands factorization
    Use a simple grep, faster than breaking into tokens

--- lib/common/sync.sh
+++ lib/common/sync.sh
@@ -33,12 +33,8 @@ get_lftp_additions() {
     local lftp_output_file=$1; shift
     local local_base=$1; shift
 
-    local line
-    while read line; do
-        if _is_lftp_addition "$line"; then
-            _get_lftp_file_addition "$line" | sed -e "s#^$local_base/##"
-        fi
-    done < $lftp_output_file
+    grep "^get -O " $lftp_output_file | _parse_lftp_file_addition_line | \
+        sed -e "s#^$local_base/##"
 }
 export -f get_lftp_additions
 
@@ -49,56 +45,23 @@ get_lftp_deletions() {
     local lftp_output_file=$1; shift
     local local_base=$1; shift
 
-    local line
-    while read line; do
-        if _is_lftp_deletion "$line"; then
-            _get_lftp_file_deletion "$line" | sed -e "s#^file:$local_base/##"
-        fi
-    done < $lftp_output_file
+    grep "^rm " $lftp_output_file | cut -d' ' -f2 | \
+        sed -e "s#^file:$local_base/##"
 }
 export -f get_lftp_deletions
 
-# returns file in lftp itemized line
-# $1 - line
-_get_lftp_file_addition() {
-    local line="$1"; shift
-    line=(${line// / })
-    # sample line is:
-    # get -O /tmp/argo/dac/csio/2900322 ftp://ftp.ifremer.fr/ifremer/argo/dac/csio/2900322/2900322_Rtraj.nc
-    # we take the basename from the `ftp://` part and add it to the
-    # /tmp/argo/dac/... part
-    local path_basename=`basename ${line[3]}`
-    echo ${line[2]}/$path_basename
-    unset line
+# returns file from lftp itemized addition line
+# STDIN - line
+_parse_lftp_file_addition_line() {
+    local line
+    while read line; do
+        line=(${line// / })
+        # sample line is:
+        # get -O /tmp/argo/dac/csio/2900322 ftp://ftp.ifremer.fr/ifremer/argo/dac/csio/2900322/2900322_Rtraj.nc
+        # we take the basename from the `ftp://` part and add it to the
+        # /tmp/argo/dac/... part
+        local path_basename=`basename ${line[3]}`
+        echo ${line[2]}/$path_basename
+    done
 }
-export -f _get_lftp_file_addition
-
-# returns file in lftp itemized line
-# $1 - line
-_get_lftp_file_deletion() {
-    local line="$1"; shift
-    line=(${line// / })
-    echo ${line[1]}; unset line
-    # equivalent to `echo $line | cut -d' ' -f2` but faster!
-}
-export -f _get_lftp_file_deletion
-
-# return 0 (true) if line is lftp deletion
-# $1 - line
-_is_lftp_deletion() {
-    local line="$1"; shift
-    [ "${line:0:3}" = "rm " ] && return 0
-
-    return 1
-}
-export -f _is_lftp_deletion
-
-# return 0 (true) if line is lftp addition
-# $1 - line
-_is_lftp_addition() {
-    local line="$1"; shift
-    [ "${line:0:7}" = "get -O " ] && return 0
-
-    return 1
-}
-export -f _is_lftp_addition
+export -f _parse_lftp_file_addition_line
