741dccf87829d6fd770889e4d3387858341ef917
Dan Fruehauf <malkodan@gmail.com>
2015-12-01 13:18:02 +1100

    Date/time comparison function

---
+++ lib/common/time.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+##################
+# TIME FUNCTIONS #
+##################
+
+# return true if rhs timestamp is after lhs. sample usage:
+# timestamp_is_increasing 2015-11-30T19:21:08 2015-11-30T19:31:09
+# returns true in this case
+# $1 - lhs timestamp (ISO8601)
+# $2 - rhs timestamp (ISO8601)
+timestamp_is_increasing() {
+    local lhs=`date -d "$1" "+%s"`; shift
+    local rhs=`date -d "$1" "+%s"`; shift
+    [ $rhs -gt $lhs ]
+}
+export -f timestamp_is_increasing
