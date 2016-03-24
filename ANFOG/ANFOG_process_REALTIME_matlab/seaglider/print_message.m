387c18a2c71329823d3518bc70081e6ba5855d49
bpasquer <benedicte.pasquer@utas.edu.au>
2016-03-24 11:43:58 +1100

    ANFOG_RT : Clean-up obsolete matlab and bash scripts for realtime processing(pre-pipeline)

--- ANFOG/ANFOG_process_REALTIME_matlab/seaglider/print_message.m
+++
@@ -1,9 +0,0 @@
-function print_message(filename,msg,varargin)
-
-fid_w = fopen(filename,'a');
-if size(varargin,2) > 0 
-    fprintf(fid_w,'%s %s %s \r\n',datestr(clock),msg,varargin{1});
-else
-    fprintf(fid_w,'%s %s \r\n',datestr(clock),msg);
-end
-fclose(fid_w);
