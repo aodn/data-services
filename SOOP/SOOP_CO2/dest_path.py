219fe14864dd570991cd5e1f8eea0800c3922316
lbesnard <laurent.besnard@utas.edu.au>
2016-04-22 15:35:51 +1000

    SOOP CO2 : add processing of Future Reef Map project data +  Refactoring and updating of dest_path and incoming handler  - updating handling of error  - changed class soop_co2_dest_path into a function in dest_path

--- SOOP/SOOP_CO2/dest_path.py
+++ SOOP/SOOP_CO2/dest_path.py
@@ -7,54 +7,106 @@ sys.path.insert(0, os.path.join(os.environ.get('DATA_SERVICES_DIR'), 'lib'))
 from python.ship_callsign import ship_callsign_list
 
 
-class soop_co2_dest_path:
+def ships():
+    return ship_callsign_list()
 
-    def __init__(self):
-        self.ships = ship_callsign_list()
+def def_project(nc_file):
+    """
+    # script deal with either IMOS or Future_Reef_MAP project files
+    # eg : IMOS_SOOP-CO2_GST_20121027T045200Z_VLHJ_FV01.nc
+    # eg : FutureReefMap_GST_20140530T185029Z_9V2768_FV01.nc
+    # <Project_Name>_<<Facility-Code>>_<Data-Code>_<Start-date>_<Platform-Code>_FV<File-Version>.nc
+    """
+    nc_file_basename = os.path.basename(nc_file)
+    file = nc_file_basename.split("_")
 
-    def dest_path(self, nc_file):
-        """
-        # eg : IMOS_SOOP-CO2_GST_20121027T045200Z_VLHJ_FV01.nc
-        # IMOS_<Facility-Code>_<Data-Code>_<Start-date>_<Platform-Code>_FV<File-Version>.nc
-        """
-        nc_file_basename = os.path.basename(nc_file)
-        file = nc_file_basename.split("_")
+    project = file[0] # <Project-Name>
+    return project
 
-        facility = file[1] # <Facility-Code>
+def soop_co2_dest_path(nc_file):
+    """
+    # eg : IMOS_SOOP-CO2_GST_20121027T045200Z_VLHJ_FV01.nc
+    # IMOS_<Facility-Code>_<Data-Code>_<Start-date>_<Platform-Code>_FV<File-Version>.nc
+    """
+    project = "IMOS"
 
-        # the file name must have at least 6 component parts to be valid
-        if len(file) > 5:
+    nc_file_basename = os.path.basename(nc_file)
+    file             = nc_file_basename.split("_")
 
-            year = file[3][:4] # year out of <Start-date>
+    # the file name must have at least 6 component parts to be valid
+    if len(file) > 5:
+        facility = file[1]
+        year     = file[3][:4] # year out of <Start-date>
 
-            # check for the code in the ships
-            code = file[4]
-            if code in self.ships:
+        # check for the code in the ships
+        code = file[4]
+        if code in ships():
+            platform = code + "_" + ships()[code]
 
-                platform = code + "_" + self.ships[code]
-
-                # open the file
-                try:
-                    F = Dataset(nc_file, mode='r')
-                except:
-                    print >>sys.stderr, "Failed to open NetCDF file '%s'" % nc_file
-                    return None
-                # add cruise_id
-                cruise_id = getattr (F, 'cruise_id', '')
-
-                F.close()
-
-                if not cruise_id:
-                    print >>sys.stderr, "File '%s' has no cruise_id attribute" % nc_file
-                    return None
-
-                target_dir = facility + os.path.sep + platform + os.path.sep + year + os.path.sep + cruise_id
-
-                return target_dir
-
-            else:
-                print >>sys.stderr, "Hierarchy not created for '%s'" % nc_file
+            # open the file
+            try:
+                F = Dataset(nc_file, mode='r')
+            except:
+                print >>sys.stderr, "Failed to open NetCDF file '%s'" % nc_file
                 return None
+            # get cruise_id
+            cruise_id = getattr (F, 'cruise_id', '')
+
+            F.close()
+
+            if not cruise_id:
+                print >>sys.stderr, "File '%s' has no cruise_id attribute" % nc_file
+                return None
+
+            target_dir = os.path.join(project, facility[:4], facility, platform, year, cruise_id)
+            return target_dir
+
+        else:
+            print >>sys.stderr, "Hierarchy not created for '%s'" % nc_file
+            return None
+
+
+def future_reef_map_dest_path(nc_file):
+    """
+    # eg : FutureReefMap_GST_20140530T185029Z_9V2768_FV01.nc
+    # <ProjectName>_<Data-Code>_<Start-date>_<Platform-Code>_FV<File-Version>.nc
+    """
+    project          = "Future_Reef_MAP"
+    data_type        = "underway"
+    nc_file_basename = os.path.basename(nc_file)
+    file             = nc_file_basename.split("_")
+
+    # the file name must have at least 5 component parts to be valid
+    if len(file) > 4:
+
+        year = file[2][:4] # year out of <Start-date>
+
+        # check code in ships
+        code = file[3]
+
+        if code in ships():
+            platform = ships()[code]
+
+        # open the file
+        try:
+            F = Dataset(nc_file, mode='r')
+        except:
+            print >>sys.stderr, "Failed to open NetCDF file '%s'" % nc_file
+            return None
+        # get cruise_id
+        cruise_id = getattr (F, 'cruise_id', '')
+        F.close()
+
+        if not cruise_id:
+            print >>sys.stderr, "File '%s' has no cruise_id attribute" % nc_file
+            return None
+
+        target_dir = os.path.join(project, data_type, platform, year, cruise_id)
+        return target_dir
+
+    else:
+        print >>sys.stderr, "Hierarchy not created for '%s'" % nc_file
+        return None
 
 
 if __name__=='__main__':
@@ -63,12 +115,17 @@ if __name__=='__main__':
         print >>sys.stderr, 'No filename specified!'
         exit(1)
 
-    dest_path = soop_co2_dest_path()
+    project = def_project(sys.argv[1])
 
-    answer = dest_path.dest_path(sys.argv[1])
-
-    if not answer:
+    if project == 'IMOS':
+        dest_path = soop_co2_dest_path(sys.argv[1])
+    elif project == 'FutureReefMap':
+        dest_path = future_reef_map_dest_path(sys.argv[1])
+    else:
         exit(1)
 
-    print answer
+    if not dest_path:
+        exit(1)
+
+    print dest_path
     exit(0)
--- SOOP/SOOP_CO2/incoming_handler.sh
+++ SOOP/SOOP_CO2/incoming_handler.sh
@@ -5,12 +5,20 @@ export SCRIPTPATH="$DATA_SERVICES_DIR/SOOP/SOOP_CO2"
 
 declare -r BACKUP_RECIPIENT=benedicte.pasquer@utas.edu.au
 
-# is_soop_co2_file
-# check that the file belongs to SOOP_CO2 subfacility
+# is_imos_soop_co2_file
+# check that the file from IMOS SOOP_CO2
 # $1 - file name
-is_soop_co2_file() {
+is_imos_soop_co2_file() {
     local file=`basename $1`; shift
     echo $file | egrep -q '^IMOS_SOOP-CO2_'
+}
+
+# is_future_reef_map_file
+# check that the file belongs toFuture Reef MAP project
+# $1 - file name
+is_future_reef_map_file() {
+    local file=`basename $1`; shift
+    echo $file | egrep -q '^FutureReefMap_'
 }
 
 # main
@@ -21,33 +29,60 @@ main() {
 
     local tmp_dir=`mktemp -d`
     chmod a+rx $tmp_dir
-    unzip -q -u -o $file -d $tmp_dir || file_error "Error unzipping"
+    local tmp_zip_manifest=`mktemp`
+
+    unzip_file $file $tmp_dir $tmp_zip_manifest
+    if [ $? -ne 0 ]; then
+        rm -f $tmp_zip_manifest
+        rm -rf --preserve-root $tmp_dir
+        file_error "Error unzipping"
+    fi
 
     local nc_file
-    nc_file=`find $tmp_dir/. -name "*.nc" | head -1` || file_error "Cannot find NetCDF file in zip bundle"
-
-    check_netcdf $nc_file || file_error_and_report_to_uploader $BACKUP_RECIPIENT "Not a valid NetCDF file"
-    # check_netcdf_cf   $file || file_error_and_report_to_uploader $BACKUP_RECIPIENT "File is not CF compliant"
-    # check_netcdf_imos $file || file_error_and_report_to_uploader $BACKUP_RECIPIENT "File is not IMOS compliant"
+    nc_file=`grep ".*.nc" $tmp_zip_manifest | head -1`
+    if [ $? -ne 0 ]; then
+        rm -f $tmp_zip_manifest
+        rm -rf --preserve-root $tmp_dir
+        file_error "Cannot find NetCDF file in zip bundle"
+    fi
 
     log_info "Processing '$nc_file'"
-    echo "" | notify_by_email $BACKUP_RECIPIENT "Processing new SOOP_CO2 file '$nc_file'"
+
+    local tmp_nc_file=`make_writable_copy $tmp_dir/$nc_file`
+
+    local tmp_nc_file_with_sig
+    local checks='cf imos'
+
+    if is_imos_soop_co2_file $tmp_nc_file; then
+        tmp_nc_file_with_sig=`trigger_checkers_and_add_signature $tmp_nc_file $BACKUP_RECIPIENT $checks`
+    elif is_future_reef_map_file $tmp_nc_file; then
+        checks='cf'
+        tmp_nc_file_with_sig=`trigger_checkers_and_add_signature $tmp_nc_file $BACKUP_RECIPIENT $checks`
+    else
+        file_error "Not an underway CO2 file '$nc_file'"
+        rm -f $tmp_zip_manifest $tmp_nc_file_with_sig $tmp_nc_file
+        rm -rf --preserve-root $tmp_dir
+    fi
+
+    tmp_nc_file=$tmp_nc_file_with_sig
+
+    echo "" | notify_by_email $BACKUP_RECIPIENT "Processing new underway CO2 file '$nc_file'"
 
     local path
-    path=`$SCRIPTPATH/dest_path.py $nc_file` || file_error "Cannot generate path for NetCDF file"
+    path=`$SCRIPTPATH/dest_path.py $tmp_nc_file` || file_error "Cannot generate path for `basename $tmp_nc_file`"
 
-    # this file will need indexing, so use s3_put
-    s3_put $nc_file IMOS/SOOP/$path/`basename $nc_file`
+    s3_put $tmp_nc_file $path/`basename $nc_file` && rm -f $tmp_dir/$nc_file
 
     local extracted_file
     for extracted_file in `find $tmp_dir -type f`; do
         local file_basename=`basename $extracted_file`
-        s3_put_no_index $extracted_file IMOS/SOOP/$path/$file_basename
+        s3_put_no_index $extracted_file $path/$file_basename
     done
 
     rm -f $file # remove zip file
     #Dangerous, but necessary, since there might be a hierarchy in the zip file provided
     rm -rf --preserve-root $tmp_dir
 }
+
 
 main "$@"
