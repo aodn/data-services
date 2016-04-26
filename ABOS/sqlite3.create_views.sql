78a7e0e30a5052b7731c526b0bf7e545a09ceac4
mhidas <marty.hidas@utas.edu.au>
2016-04-26 11:57:51 +1000

    ABOS: remove obsolete code

--- ABOS/sqlite3.create_views.sql
+++
@@ -1,7 +0,0 @@
-
-SELECT 
-  min(staging.filename) AS first_file,
-  staging.dest_path
-FROM staging LEFT JOIN opendap USING (filename)
-WHERE opendap.source_path IS NOT NULL
-GROUP BY staging.dest_path;
