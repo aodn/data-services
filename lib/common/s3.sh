eebfe1c92ca7e3202f18cf450f977fe44ef023d7
lbesnard <laurent.besnard@utas.edu.au>
2016-04-28 11:25:32 +1000

    s3 functions - list recursively objects and download
     * created 2 functions to ease the process of folder restructuration,
       download ...
     * file objects can be listed recursively with s3_ls_recur function
     * file objects can be downloaded recursively with
       s3_dl_recur_latest_version function

--- lib/common/s3.sh
+++ lib/common/s3.sh
@@ -89,3 +89,53 @@ s3_ls() {
     $S3CMD ls $S3_BUCKET/$path/ | tr -s " " | cut -d' ' -f4 | sed -e "s#^$S3_BUCKET/$path/##"
 }
 export -f s3_ls
+
+# list recursively a folder on s3 and returns list of fullpath of file objects
+# $1 - path of folder on s3
+s3_ls_recur() {
+    local folder=${1%/}; shift # remove trailing slash
+    local object
+
+    for object in `s3lsv -a -b imos-data -k $folder/`; do
+        # check if object is a folder
+        if `echo "$object" | grep -E -q '\/$'`; then
+            s3_ls_recur "$object"
+        else
+            echo "$object" | grep -o -E "$folder.*$"
+        fi
+    done
+}
+export -f s3_ls_recur
+
+# download recursively s3 objects from a given folder object
+# ex: s3_dl_recur_latest_version UWA /tmp/download_test
+# $1 - path of folder on s3
+# $2 - path of output folder where files will be downloaded keeping same structure
+s3_dl_recur_latest_version() {
+    local folder=$1; shift
+    local output_folder=$1; shift
+    [ -z "$output_folder" ] && { echo "output_folder is empty"; return 1; }
+    mkdir -p $output_folder  || return 1
+
+    # only keeping uniq filename objects
+    local file_list=`s3_ls_recur $folder | uniq | sort`
+    local object
+    local obj_info
+
+    for object in $file_list; do
+        obj_info=`s3lsv -a -b imos-data -k $object`
+        # if more than one version of same object, we reverse sort the second column(date) and take the first line
+        # i.e. the lastest object version
+        # date format is yyyy-mm-ddTHH:MM:SS
+        obj_info=`echo $obj_info | sort -b -r -k 2.1,2.4 -k 2.6,2.7 -k 2.9,2.10 -k 2.12,2.13 -k 2.15,2.16 -k 2.18,2.19 | head -1`
+
+        obj_vers=`echo "$obj_info" | awk {'print $1'}`
+        obj_name=`echo "$obj_info" | awk {'print $3'}`
+        obj_date=`echo "$obj_info" | awk {'print $2'}`
+
+        mkdir -p `dirname $output_folder/$obj_name`
+        echo "Downloading $obj_name"
+        s3lsv -a -b imos-data -k $obj_name -o $output_folder/$obj_name -v $obj_vers
+    done
+}
+export -f s3_dl_recur_latest_version
