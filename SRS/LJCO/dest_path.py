eb5791c346b17285c9724fec4dd4d9c4ef22931c
lbesnard <laurent.besnard@utas.edu.au>
2016-07-20 17:35:16 +1000

    update dest_path for ljco new products

--- SRS/LJCO/dest_path.py
+++ SRS/LJCO/dest_path.py
@@ -16,24 +16,26 @@ def create_file_hierarchy(netcdf_file_path):
     netcdf_filename  = remove_creation_date_from_filename(netcdf_filename)
 
     # looking for product_name
-    m = re.search('^IMOS_SRS-OC-LJCO_KOSTUZ_(.*)_SRC_FV(.*)\.nc$',
+    m = re.search('^IMOS_SRS-OC-LJCO_(.*)_(.*)_SRC_FV(.*)\.nc$',
                   netcdf_filename)
+
     if m is None:
         return None
 
-    if m.group(2) == '01_WQM-hourly':
-        product_dir = 'WQM-hourly'
-    elif m.group(2) == '01_Weather-monthly':
-        product_dir = 'Weather-monthly'
-    elif m.group(2) == '02_WQM-daily':
-        product_dir = 'WQM-daily'
-    else:
-        return None
+    product_type_ls     = re.compile('ACS|EcoTriplet|BB9|HyperOCR|WQM')
+    product_type_netcdf = product_type_ls.findall(netcdf_filename)
 
-    year = int(m.group(1)[0:4])
-    if product_dir == 'WQM-hourly':
-        month                = int(m.group(1)[4:6])
-        day                  = int(m.group(1)[6:8])
+    product_temp_ls     = re.compile('hourly|daily|monthly')
+    product_temp_netcdf = product_temp_ls.findall(netcdf_filename)
+
+    if product_type_netcdf == [] or product_temp_netcdf == []:
+        return None
+    product_dir = '%s-%s' % (product_type_netcdf[0], product_temp_netcdf[0])
+
+    year = int(m.group(2)[0:4])
+    if 'hourly' in product_dir:
+        month                = int(m.group(2)[4:6])
+        day                  = int(m.group(2)[6:8])
         relative_netcdf_path = os.path.join(ljco_wqm_dir, product_dir, '%d' % year,
                                             '%02d' % month, '%02d' % day, netcdf_filename)
     else:
