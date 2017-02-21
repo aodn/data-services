dd0d5cec3f0657aa49e2a77a015f25733f23e2fb
lbesnard <laurent.besnard@utas.edu.au>
2017-02-21 17:07:41 +1100

    SOOP XBT DM - new uploaded files dont need to be modified by incoming handler to be imos compliant

--- SOOP/SOOP_ASF_SST_XBT/soop_xbt_pipeline/soop_xbt_incoming_handler_dm.sh
+++ SOOP/SOOP_ASF_SST_XBT/soop_xbt_pipeline/soop_xbt_incoming_handler_dm.sh
@@ -23,14 +23,8 @@ main() {
 
     regex_filter "$regex" $file || file_error "Did not pass regex filter '$regex'"
 
-    # modify original file to pass the checker
-    local tmp_file_compliant=`mktemp`
-    $DATA_SERVICES_DIR/SOOP/SOOP_ASF_SST_XBT/soop_xbt_pipeline/soop_xbt_netcdf_compliance.sh \
-        $file $tmp_file_compliant
-
     local tmp_file
-    tmp_file=`trigger_checkers_and_add_signature $tmp_file_compliant $DEFAULT_BACKUP_RECIPIENT $checks` || return 1
-    rm -f $tmp_file_compliant
+    tmp_file=`trigger_checkers_and_add_signature $file $DEFAULT_BACKUP_RECIPIENT $checks` || return 1
 
     local path_hierarchy
     path_hierarchy=`$DATA_SERVICES_DIR/$PATH_EVALUATION_EXECUTABLE $file`
