4ec5de22f4309b5f080aa07b6e26f22c41dc2129
Guillaume Galibert <guillaume.galibert@utas.edu.au>
2017-11-29 15:40:59 +1100

    SSTAARS.nc and SSTAARS_daily_fit.nc are now handled by pipeline yet not indexed by talend harvester.

--- CSIRO/SSTAARS/dest_path.py
+++ CSIRO/SSTAARS/dest_path.py
@@ -2,12 +2,25 @@
 # -*- coding: utf-8 -*-
 """
 Returns the relative path of a SSTAARS NetCDF file
-author Laurent Besnard, laurent.besnard@utas.edu.au
 """
 
 import os
 import re
 import sys
+
+def create_file_hierarchy(netcdf_file_path):
+    sstaars_alt_dir = os.path.join('CSIRO', 'Climatology', 'SSTAARS', '2017')
+    sstaars_aodn_dir = os.path.join(sstaars_alt_dir, 'AODN-product')
+    netcdf_file_name = os.path.basename(netcdf_file_path)
+
+    regex_daily_files = re.compile('SSTAARS_daily_fit_[0-9]{3}\.nc')
+
+    if netcdf_file_name == 'SSTAARS.nc'
+        return sstaars_alt_dir
+    elif (netcdf_file_name == 'SSTAARS_daily_fit.nc') or re.match(regex_daily_files, netcdf_file_name)
+        return sstaars_aodn_dir
+    else:
+        return None
 
 if __name__ == '__main__':
     # read filename from command line
@@ -15,5 +28,10 @@ if __name__ == '__main__':
         print >>sys.stderr, 'No filename specified!'
         exit(1)
 
-    print 'CSIRO/Climatology/SSTAARS/2017/AODN-product/%s' % (os.path.basename(sys.argv[1]))
+    destination_path = create_file_hierarchy(sys.argv[1])
+
+    if not destination_path:
+        exit(1)
+
+    print destination_path
     exit(0)
