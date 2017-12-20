2ba16b884c7ab847a1fd41f45a0cd8159975749d
lbesnard <laurent.besnard@utas.edu.au>
2017-12-20 13:03:32 +1100

    Compliance Checker V3.1 - change the testing method to be based on watch d script info

--- lib/python/cc_v3.1_testing.py
+++ lib/python/cc_v3.1_testing.py
@@ -2,51 +2,53 @@
 # -*- coding: utf-8 -*-
 """ This script was done to quickly generate a summary of the current state of
 success and failure of tests on netcdf files with the latest version of the IOOS
-compliance checker v3.1 . This script can be used as part of a bash loop to test
-NetCDF files and create a CSV output such as:
-for f in `find . -type f -iname '*.nc'`; do ./cc_v3.1_testing.py $f >> checker3_result.csv ; done
+compliance checker v3.1 .
+Files to be tested on are currently in $WIP_DIR/checker3_testing/ on AWS10
 
-The current selection of files is available on AWS10, under $WIP_DIR/checker
+./cc_v3.1_testing.py $WIP_DIR/checker3_testing/ >> checker3_result.csv ; done
+
 """
 
-import re
+import csv
+import os
 import sys
 
 from util import pass_netcdf_checker
-from netCDF4 import Dataset
 
 
-def list_nc_checks_to_perform(netcdf_file_path):
-    netcdf_file_obj = Dataset(netcdf_file_path, 'r', format='NETCDF4')
-    if hasattr(netcdf_file_obj, 'Conventions'):
-        conventions     = netcdf_file_obj.Conventions
-    else:
+def read_facility_csv(csv_file_path):
+    reader = csv.DictReader(open(csv_file_path))
+    result = {}
+    for row in reader:
+        result[row['JSON watch.d']] = row['check']
+
+    return result
+
+
+def list_nc_files(root_path):
+    file_list = []
+    for root, dirs, files in os.walk(root_path):
+        for name in files:
+            if name.endswith((".nc")):
+                file_list.append(os.path.abspath(os.path.join(root_path, root, name)))
+    return file_list
+
+
+def list_nc_checks_to_perform(netcdf_file_path, facility_csv_file_path):
+    facility_checks = read_facility_csv(facility_csv_file_path)
+    file_checks     = facility_checks[os.path.basename(os.path.dirname(netcdf_file_path))]
+
+    if file_checks == "NO CHECK":
         return []
 
-    tests = []
-    if 'IMOS-1.3' in conventions:
-        tests.append('imos:1.3')
-    if 'IMOS-1.4' in conventions:
-        tests.append('imos:1.4')
-    if 'CF-1.6' in conventions:
-        tests.append('cf:1.6')
-
-    return tests
+    return file_checks.split()
 
 
-def get_sub_facility_name(netcdf_file_path):
-    m = re.search('IMOS_(.*?)_(.*)\.nc$', netcdf_file_path)
-    if hasattr(m, 'group'):
-        return m.group(1)
-    else:
-        exit(0)
-
-
-def testing(netcdf_file_path):
-    tests = list_nc_checks_to_perform(netcdf_file_path)
-    facility = get_sub_facility_name(netcdf_file_path)
+def testing(netcdf_file_path, facility_csv_file_path):
+    tests = list_nc_checks_to_perform(netcdf_file_path, facility_csv_file_path)
+    facility = os.path.basename(os.path.dirname(netcdf_file_path))
     if tests == []:
-        return '%s,%s,%s' % (netcdf_file_path, 'no Conventions att in NC', facility)
+        return '%s,%s,%s' % (netcdf_file_path, 'no check to perform', facility)
 
     res_str = ''
     for test in tests:
@@ -61,9 +63,13 @@ def testing(netcdf_file_path):
 
 
 if __name__  == '__main__':
-    # read filename from command line
+    # read path where testing data is from command line
+
     if len(sys.argv) < 2:
         print >>sys.stderr, 'No filename specified!'
         exit(1)
 
-    print testing(sys.argv[1])
+    root_path    = sys.argv[1]
+    nc_file_list = list_nc_files(root_path)
+    for f in nc_file_list:
+        print testing(f, os.path.join(root_path, 'facility_checks.csv'))
