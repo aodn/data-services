ANMN - burst average - fix issues #781
 * modify code to be more generic and not relying on TEMP to exist
 * using instrument_sample_interval global att value when exist

ANMN/burst_averaged_product/burst_average.py
@@ -159,7 +159,7 @@ def burst_average_data(time_values, var_values, var_qc_exclusion):
     # look for start index of each burst, ie the index of each new spike of
     # difft variable
     idx_spike = [0] # initialise first index
-    spikes    = np.where(difft > 1)[0] + 1
+    spikes    = np.where(difft > INSTRUMENT_SAMPLE_INTERVAL)[0] + 1
     idx_spike.extend(spikes)
 
     # initialise arrays
@@ -215,7 +215,7 @@ def generate_netcdf_burst_filename(input_netcdf_file_path, burst_vars):
     pattern           = re.compile("^(IMOS_.*)_([0-9]{8}T[0-9]{6}Z)_(.*)_(FV0[0-9])_(.*)_END")
     match_group       = pattern.match(input_netcdf_name)
 
-    time_burst_vals = burst_vars['TEMP']['time_mean']
+    time_burst_vals = burst_vars[burst_vars.keys()[0]]['time_mean']
     time_min        = num2date(time_burst_vals, netcdf_file_obj['TIME'].units, netcdf_file_obj['TIME'].calendar).min().strftime('%Y%m%dT%H%M%SZ')
     time_max        = num2date(time_burst_vals, netcdf_file_obj['TIME'].units, netcdf_file_obj['TIME'].calendar).max().strftime('%Y%m%dT%H%M%SZ')
     burst_filename  = "%s_%s_%s_FV02_%s-burst-averaged_END-%s_C-%s.nc" % (match_group.group(1), time_min, \
@@ -231,8 +231,15 @@ def create_burst_average_netcdf(input_netcdf_file_path, output_dir):
     """
     input_file_rel_path = get_input_file_rel_path(input_netcdf_file_path)
     input_netcdf_obj    = Dataset(input_netcdf_file_path, 'r')
+
+    global INSTRUMENT_SAMPLE_INTERVAL
+    if hasattr(input_netcdf_obj, 'instrument_sample_interval'):
+        INSTRUMENT_SAMPLE_INTERVAL = input_netcdf_obj.instrument_sample_interval
+    else:
+        INSTRUMENT_SAMPLE_INTERVAL = 1
+
     burst_vars          = create_burst_average_var(input_netcdf_obj)
-    time_burst_vals     = burst_vars['TEMP']['time_mean']
+    time_burst_vals     = burst_vars[burst_vars.keys()[0]]['time_mean']
     tmp_netcdf_dir      = tempfile.mkdtemp()
 
     output_netcdf_file_path = os.path.join(tmp_netcdf_dir, generate_netcdf_burst_filename(input_netcdf_file_path, burst_vars))