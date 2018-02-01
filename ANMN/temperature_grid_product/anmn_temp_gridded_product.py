b10b8a46b9f1fbb04229969d7898f00c07407248
Guillaume Galibert <guillaume.galibert@utas.edu.au>
2018-02-01 15:38:44 +1100

    create_temp_interp_gridded now does binning using numpy.histogram(). In addition vertical interpolation is now performed with numpy.interp(). temp_gridded is now a numpy array and is returned instead of the panda dataframe. Changes in code done accordingly.

--- ANMN/temperature_grid_product/anmn_temp_gridded_product.py
+++ ANMN/temperature_grid_product/anmn_temp_gridded_product.py
@@ -21,7 +21,6 @@ Author: laurent.besnard@utas.edu.au
 
 
 import argparse
-import bisect
 import os
 import re
 import shutil
@@ -231,48 +230,59 @@ def create_temp_interp_gridded(time_1d_interp, depth_1d_interp, temp_values, tim
     """
     create the interpolated gridded temperature data. The reference grid is time_1d_interp,
     and depth_1d_interp.
-    We first look for temp values within a time bin, average them, and then do a
+    We first look for temp values within time bins, average them, and then do a
     linear interpolation over the depth
-    """
+    """    
     # initialise with nan
-    temp_gridded = [[np.nan]*len(time_1d_interp) for _ in range(len(depth_1d_interp))]
-    n_depth_file = len(temp_values)
-    for id in range(n_depth_file):
-        # binning of time
-        time_delta = (time_1d_interp[1]- time_1d_interp[0])
-        for t_gridded_idx in range(len(time_1d_interp)-1):
-            lower = bisect.bisect_left(time_values[id], time_1d_interp[t_gridded_idx] - time_delta/2 )
-            upper = bisect.bisect_left(time_values[id], time_1d_interp[t_gridded_idx + 1] - time_delta/2)-1
-            time_indexes_cell = range(lower, upper + 1)
+    temp_gridded = np.array([[np.nan]*len(time_1d_interp) for _ in range(len(depth_1d_interp))])
 
-            depth_uniq_time = depth_values[id][time_indexes_cell]
-            depth_idx_eq    = find_closest(np.array(depth_1d_interp), depth_uniq_time)
+    n_file = len(temp_values)
+    n_time = len(time_1d_interp)
+    
+    temp_binned_array  = []
+    depth_binned_array = []
+    
+    time_delta = (time_1d_interp[1]- time_1d_interp[0])
+        
+    time_bins_start = []
+    for j in range(n_time):
+        time_bins_start.append(time_1d_interp[j] - time_delta/2) # time_1d_interp sits in the centre of the bin
 
-            for array_index, d_gridded_idx in enumerate(depth_idx_eq):
-                # idx_time can be linked to more than one real value for one
-                # cell in the gridded product, so we do an average of those
-                temp_gridded[d_gridded_idx][t_gridded_idx] = np.nanmean([temp_gridded[d_gridded_idx][t_gridded_idx], temp_values[id][time_indexes_cell][array_index]])
+    time_bins_start.append(time_1d_interp[j] + time_delta/2) # add last value
 
-    # remove NaN off depth edges of pandas grid
-    def trim_nans_off_rows(df):
-        first_idx = df.first_valid_index()
-        last_idx  = df.last_valid_index()
-        df        = df.loc[first_idx:last_idx]
-        return df
+    # histogram doesn't work with datetime so we need to use timestamps in seconds since a reference date
+    to_timestamp = np.vectorize(lambda x: (x - datetime(1970, 1, 1)).total_seconds())
+    
+    timestamp_bins_start = to_timestamp(time_bins_start)
+    
+    # temporal binning per dataset
+    for i_file in range(n_file):        
+        timestamp_values = to_timestamp(time_values[i_file])
+        
+        time_hist = np.histogram(timestamp_values, timestamp_bins_start)[0] # sometimes there is no data in a bin -> 0
+        
+        temp_binned  = np.histogram(timestamp_values, timestamp_bins_start, weights=temp_values[i_file]) [0] / time_hist # when there is no data in a bin -> 0 divided by 0 yields a NaN
+        depth_binned = np.histogram(timestamp_values, timestamp_bins_start, weights=depth_values[i_file])[0] / time_hist
+        
+        temp_binned_array.append(temp_binned)
+        depth_binned_array.append(depth_binned)
 
-    df = pd.DataFrame(temp_gridded, columns=time_1d_interp, index=depth_1d_interp)
-    try:
-        df = df.interpolate(method='slinear', axis=0, limit_direction='both') #depth
-        df = df.interpolate(method='linear', axis=1, limit=1)
-    except Exception as err:
-        logger.error('error with interpolation method - %s' % err)
-        cleaning_err_exit()
-
-    df = trim_nans_off_rows(df)
-    df = df.transpose()
-    df = trim_nans_off_rows(df)
-    df = df.transpose()
-    return df
+    # vertical interpolation per time stamp
+    for j in range(n_time):
+        temp_binned  = np.array([row[j] for row in temp_binned_array])
+        depth_binned = np.array([row[j] for row in depth_binned_array])
+        
+        temp_binned  = temp_binned [~np.isnan(temp_binned)]
+        depth_binned = depth_binned[~np.isnan(depth_binned)]
+        
+        # we need to sort temp and depth by increasing depths before we can interpolate
+        temp_binned = [x for _,x in sorted(zip(depth_binned,temp_binned))]
+        depth_binned.sort()
+        
+        # we only want to interpolate what's between the depth_binned range, what is below or above is nan
+        temp_gridded[:,j] = np.interp(depth_1d_interp, depth_binned, temp_binned, left=np.nan, right=np.nan)
+    
+    return temp_gridded
 
 def list_instrument_nominal_depth(nc_file_list):
     """ return a list of nominal_depth gatt from the nc files"""
@@ -284,7 +294,7 @@ def list_instrument_nominal_depth(nc_file_list):
     instrument_nominal_depth.sort()
     return instrument_nominal_depth
 
-def generate_fv02_filename(df, nc_file_list):
+def generate_fv02_filename(time_1d_interp, nc_file_list):
     """ return the file name only of the FV02 product """
     netcdf_file_obj   = Dataset(nc_file_list[0], 'r')
     site_code         = netcdf_file_obj.site_code
@@ -293,9 +303,8 @@ def generate_fv02_filename(df, nc_file_list):
     pattern           = re.compile("^(IMOS_.*)_([A-Z].*)_([0-9]{8}T[0-9]{6}Z)_(.*)_FV01_(.*)_END")
     match_group       = pattern.match(input_netcdf_name)
 
-    time_values = transform_datetime64_datetime(df.columns.values)
-    time_start  = min(time_values).strftime('%Y%m%dT%H%M%SZ')
-    time_end    = max(time_values).strftime('%Y%m%dT%H%M%SZ')
+    time_start  = min(time_1d_interp).strftime('%Y%m%dT%H%M%SZ')
+    time_end    = max(time_1d_interp).strftime('%Y%m%dT%H%M%SZ')
 
     output_netcdf_name = '%s_T_%s_%s_FV02_%s_gridded_END-%s.nc' % (match_group.group(1), time_start,
                                                                     site_code, deployment_code, time_end)
@@ -303,18 +312,10 @@ def generate_fv02_filename(df, nc_file_list):
     netcdf_file_obj.close()
     return output_netcdf_name
 
-def transform_datetime64_datetime(datetime64_object):
-    """ The pandas date object is turned into a datetime64 type. This creates an issue when
-    doing a normal datetime operation which is fixed by running this function """
-    # http://stackoverflow.com/questions/13703720/converting-between-datetime-timestamp-and-datetime64
-    ns          = 1e-9 # nanoseconds
-    time_values = [datetime.utcfromtimestamp(x.astype(int) * ns) for x  in datetime64_object]
-    return time_values
-
-def generate_fv02_netcdf(df, nc_file_list):
+def generate_fv02_netcdf(temp_gridded, time_1d_interp, depth_1d_interp, nc_file_list):
     """ generated the FV02 temperature gridded product netcdf file """
     tmp_netcdf_dir          = tempfile.mkdtemp()
-    output_netcdf_file_path = os.path.join(tmp_netcdf_dir, generate_fv02_filename(df, nc_file_list))
+    output_netcdf_file_path = os.path.join(tmp_netcdf_dir, generate_fv02_filename(time_1d_interp, nc_file_list))
 
     input_netcdf_obj               = Dataset(nc_file_list[0], 'r')
     output_netcdf_obj              = Dataset(output_netcdf_file_path, "w", format="NETCDF4")
@@ -342,8 +343,8 @@ def generate_fv02_netcdf(df, nc_file_list):
     instrument_nominal_depth = ", ".join(map(str, list_instrument_nominal_depth(nc_file_list)))
     setattr(output_netcdf_obj, 'instrument_nominal_depth', instrument_nominal_depth)
 
-    output_netcdf_obj.createDimension("TIME", len(df.columns))
-    output_netcdf_obj.createDimension("DEPTH", len(df.index))
+    output_netcdf_obj.createDimension("TIME", temp_gridded.shape[1])
+    output_netcdf_obj.createDimension("DEPTH", temp_gridded.shape[0])
     output_netcdf_obj.createDimension("LATITUDE", 1)
     output_netcdf_obj.createDimension("LONGITUDE", 1)
 
@@ -353,12 +354,12 @@ def generate_fv02_netcdf(df, nc_file_list):
     var_depth    = output_netcdf_obj.createVariable("DEPTH", "f", "DEPTH", fill_value=get_imos_parameter_info('DEPTH', '_FillValue'))
     var_lat[:]   = input_netcdf_obj['LATITUDE'][:]
     var_lon[:]   = input_netcdf_obj['LONGITUDE'][:]
-    var_depth[:] = df.index.values
+    var_depth[:] = depth_1d_interp
 
     main_var           = 'TEMP'
     fillvalue          = get_imos_parameter_info(main_var, '_FillValue')
     output_main_var    = output_netcdf_obj.createVariable(main_var, "f4", ("TIME", "DEPTH"), fill_value=fillvalue)
-    output_main_var[:] = df.values.transpose()
+    output_main_var[:] = np.transpose(temp_gridded)
     output_main_var.coordinates = "TIME LONGITUDE LATITUDE DEPTH"
 
     # add gatts and variable attributes as stored in config files
@@ -378,28 +379,26 @@ def generate_fv02_netcdf(df, nc_file_list):
     add_var_att_from_input_nc_to_output_nc('LONGITUDE')
     add_var_att_from_input_nc_to_output_nc('LATITUDE')
 
-    # convert datetime64 to datetime
-    time_values      = transform_datetime64_datetime(df.columns.values)
-    time_val_dateobj = date2num(time_values, output_netcdf_obj['TIME'].units, output_netcdf_obj['TIME'].calendar)
+    time_val_dateobj = date2num(time_1d_interp, output_netcdf_obj['TIME'].units, output_netcdf_obj['TIME'].calendar)
     var_time[:]      = time_val_dateobj
 
-    output_netcdf_obj.time_coverage_start = min(time_values).strftime('%Y-%m-%dT%H:%M:%SZ')
-    output_netcdf_obj.time_coverage_end   = max(time_values).strftime('%Y-%m-%dT%H:%M:%SZ')
+    output_netcdf_obj.time_coverage_start = min(time_1d_interp).strftime('%Y-%m-%dT%H:%M:%SZ')
+    output_netcdf_obj.time_coverage_end   = max(time_1d_interp).strftime('%Y-%m-%dT%H:%M:%SZ')
 
-    output_netcdf_obj.geospatial_vertical_min = float(df.index.values.min())
-    output_netcdf_obj.geospatial_vertical_max = float(df.index.values.max())
+    output_netcdf_obj.geospatial_vertical_min = float(np.min(depth_1d_interp))
+    output_netcdf_obj.geospatial_vertical_max = float(np.max(depth_1d_interp))
 
     abstract = ("This product aggregates Temperature logger data collected at "
                 "these nominal depths (%s) on the mooring line during the %s "
                 "deployment by averaging them temporally and interpolating them "
-                "vertically on a common grid. The grid covers from %s to %s "
+                "vertically at consistent depths. The grid covers from %s to %s "
                 "temporally and from %s to %s metres vertically. A cell is %s "
                 "minutes wide and %s metre high") % (instrument_nominal_depth,
                                                 output_netcdf_obj.deployment_code,
                                                 output_netcdf_obj.time_coverage_start,
                                                 output_netcdf_obj.time_coverage_end,
-                                                df.index.values.min(),
-                                                df.index.values.max(),
+                                                output_netcdf_obj.geospatial_vertical_min,
+                                                output_netcdf_obj.geospatial_vertical_max,
                                                 output_netcdf_obj.temporal_resolution,
                                                 output_netcdf_obj.vertical_resolution)
 
@@ -419,7 +418,7 @@ def create_fv02_product(nc_file_list):
     temp, depth, time               = get_data_in_deployment(nc_file_list)
     temp_gridded                    = create_temp_interp_gridded(time_1d_interp, depth_1d_interp, temp, time, depth)
 
-    output_file_name = generate_fv02_netcdf(temp_gridded, nc_file_list)
+    output_file_name = generate_fv02_netcdf(temp_gridded, time_1d_interp, depth_1d_interp, nc_file_list)
     return output_file_name
 
 def download_list_nc(list_url):
