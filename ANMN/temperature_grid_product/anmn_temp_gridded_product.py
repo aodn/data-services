7f7ed859cae5abae9c5ff073f142eb652e3cf0d5
Guillaume Galibert <guillaume.galibert@utas.edu.au>
2018-02-06 12:39:19 +1100

    wfs_request_matching_file_pattern() must have option url_column specified for schema anmn_ts_timeseries_map.

--- ANMN/temperature_grid_product/anmn_temp_gridded_product.py
+++ ANMN/temperature_grid_product/anmn_temp_gridded_product.py
@@ -482,7 +482,7 @@ def main(incoming_file_path, deployment_code, output_dir, plot_comparison=False)
     s3_bucket_prefix = 'https://s3-ap-southeast-2.amazonaws.com/imos-data'
     logging           = IMOSLogging()
     logger            = logging.logging_start(os.path.join(os.environ['WIP_DIR'], 'anmn_temp_grid.log'))
-    list_fv01_url     = wfs_request_matching_file_pattern('anmn_ts_timeseries_map', '%%_FV01_%s%%' % deployment_code, s3_bucket_url=True)
+    list_fv01_url     = wfs_request_matching_file_pattern('anmn_ts_timeseries_map', '%%_FV01_%s%%' % deployment_code, s3_bucket_url=True, url_column='file_url')
     previous_fv02_url = wfs_request_matching_file_pattern('anmn_all_map', '%%Temperature/gridded/%%_FV02_%s_%%gridded%%' % deployment_code)
 
     if len(previous_fv02_url) == 1:
