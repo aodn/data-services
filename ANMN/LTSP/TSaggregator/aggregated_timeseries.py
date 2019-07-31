7f8d169b370b3d1bb0ecc01d108c782b77eee907
diodon <ekleins@gmail.com>
2019-07-31 11:11:05 +1000

    add PRES and PRES_REL variables to the aggregator

--- ANMN/LTSP/TSaggregator/aggregated_timeseries.py
+++ ANMN/LTSP/TSaggregator/aggregated_timeseries.py
@@ -133,15 +133,14 @@ def main_aggregator(files_to_agg, var_to_agg):
     FILLVALUE = 999999.0
     FILLVALUEqc = 99
 
-    varlist = ['TIME', var_to_agg, var_to_agg + '_quality_control', 'DEPTH', 'DEPTH_quality_control',
-               'LONGITUDE', 'LATITUDE', 'NOMINAL_DEPTH', 'instrument_index', 'instrument_id']
-
     ## create empty DF for main and auxiliary variables
     MainDF_types = [('VAR', float),
                     ('VARqc', np.byte),
                     ('TIME', np.float64),
                     ('DEPTH', float),
                     ('DEPTH_quality_control', np.byte),
+                    ('PRES', np.float64),
+                    ('PRES_REL', np.float64),
                     ('instrument_index', int)]
 
     AuxDF_types = [('source_file', str),
@@ -162,6 +161,7 @@ def main_aggregator(files_to_agg, var_to_agg):
         ## it will open the netCDF files as a xarray Dataset
         with xr.open_dataset(file) as nc:
             varnames = list(nc.variables.keys())
+            nobs = len(nc.TIME)
 
             ## get the in-water times
             ## important to remove the timezone aware of the converted datetime object from a string
@@ -169,26 +169,48 @@ def main_aggregator(files_to_agg, var_to_agg):
             time_deployment_end = pd.to_datetime(parse(nc.attrs['time_deployment_end'])).tz_localize(None)
 
             ## Check if DEPTH is present. If not store FillValues
+            DF = pd.DataFrame({ 'VAR': nc[var_to_agg].squeeze(),
+                                'VARqc': nc[var_to_agg + '_quality_control'].squeeze(),
+                                'TIME': nc.TIME.squeeze(),
+                                'instrument_index': np.repeat(fileIndex, nobs)})
+
+            ## check for DEPTH/PRES variables in the nc and its qc flags
             if 'DEPTH' in varnames:
-                DF = pd.DataFrame({ 'VAR': nc[var_to_agg].squeeze(),
-                                    'VARqc': nc[var_to_agg + '_quality_control'].squeeze(),
-                                    'TIME': nc.TIME.squeeze(),
-                                    'DEPTH': nc.DEPTH.squeeze(),
-                                    'DEPTH_quality_control': nc.DEPTH_quality_control.squeeze(),
-                                    'instrument_index': np.repeat(fileIndex, len(nc['TIME']))})
+                DF['DEPTH'] = nc.DEPTH.squeeze()
+                if 'DEPTH_quality_control' in varnames:
+                    DF['DEPTH_quality_control'] = nc.DEPTH_quality_control.squeeze()
+                else:
+                    DF['DEPTH_quality_control'] = np.repeat(9, nobs)
             else:
-                DF = pd.DataFrame({ 'VAR': nc[var_to_agg].squeeze(),
-                                    'VARqc': nc[var_to_agg + '_quality_control'].squeeze(),
-                                    'TIME': nc.TIME.squeeze(),
-                                    'instrument_index': np.repeat(fileIndex, len(nc['TIME'])),
-                                    'DEPTH': np.repeat(FILLVALUE, len(nc['TIME'])),
-                                    'DEPTH_quality_control': np.repeat(9, len(nc['TIME']))})
+                DF['DEPTH'] = np.repeat(FILLVALUE, nobs)
+                DF['DEPTH_quality_control'] = np.repeat(9, nobs)
+
+            if 'PRES' in varnames:
+                DF['PRES'] = nc.PRES.squeeze()
+                if 'PRES_quality_control' in varnames:
+                    DF['PRES_quality_control'] = nc.PRES_quality_control.squeeze()
+                else:
+                    DF['PRES_quality_control'] = np.repeat(9, nobs)
+            else:
+                DF['PRES'] = np.repeat(FILLVALUE, nobs)
+                DF['PRES_quality_control'] = np.repeat(9, nobs)
+
+            if 'PRES_REL' in varnames:
+                DF['PRES_REL'] = nc.PRES_REL.squeeze()
+                if 'PRES_REL_quality_control' in varnames:
+                    DF['PRES_REL_quality_control'] = nc.PRES_REL_quality_control.squeeze()
+                else:
+                    DF['PRES_REL_quality_control'] = np.repeat(9, nobs)
+            else:
+                DF['PRES_REL'] = np.repeat(FILLVALUE, nobs)
+                DF['PRES_REL_quality_control'] = np.repeat(9, nobs)
+
 
             ## select only in water data
             DF = DF[(DF['TIME']>=time_deployment_start) & (DF['TIME']<=time_deployment_end)]
 
             ## append data
-            variableMainDF = pd.concat([variableMainDF, DF], ignore_index=True)
+            variableMainDF = pd.concat([variableMainDF, DF], ignore_index=True, sort=False)
 
             # append auxiliary data
             variableAuxDF = variableAuxDF.append({'source_file': file,
@@ -223,6 +245,10 @@ def main_aggregator(files_to_agg, var_to_agg):
                           'TIME':                           (['OBSERVATION'],variableMainDF['TIME'], variable_attributes['TIME']),
                           'DEPTH':                          (['OBSERVATION'],variableMainDF['DEPTH'].astype('float32'), variable_attributes['DEPTH']),
                           'DEPTH_quality_control':          (['OBSERVATION'],variableMainDF['DEPTH_quality_control'].astype(np.byte), variable_attributes['DEPTH_quality_control']),
+                          'PRES':                           (['OBSERVATION'],variableMainDF['PRES'].astype('float32'), variable_attributes['PRES']),
+                          'PRES_quality_control':           (['OBSERVATION'],variableMainDF['PRES_quality_control'].astype(np.byte), variable_attributes['PRES_quality_control']),
+                          'PRES_REL':                       (['OBSERVATION'],variableMainDF['PRES_REL'].astype('float32'), variable_attributes['PRES_REL']),
+                          'PRES_REL_quality_control':       (['OBSERVATION'],variableMainDF['PRES_REL_quality_control'].astype(np.byte), variable_attributes['PRES_REL_quality_control']),
                           'instrument_index':               (['OBSERVATION'],variableMainDF['instrument_index'].astype('int64'), variable_attributes['instrument_index']),
                           'LONGITUDE':                      (['INSTRUMENT'], variableAuxDF['LONGITUDE'].astype('float32'), variable_attributes['LONGITUDE']),
                           'LATITUDE':                       (['INSTRUMENT'], variableAuxDF['LATITUDE'].astype('float32'), variable_attributes['LATITUDE']),
@@ -255,10 +281,17 @@ if __name__ == "__main__":
     # print('number of files: %i' % len(files_to_aggregate))
 
     # # to test
+    # files_to_aggregate = ['http://thredds.aodn.org.au/thredds/dodsC/IMOS/ANMN/NRS/NRSROT/Temperature/IMOS_ANMN-NRS_TZ_20141215T160000Z_NRSROT_FV01_NRSROT-1412-SBE39-33_END-20150331T063000Z_C-20180508T001839Z.nc',
+    # 'http://thredds.aodn.org.au/thredds/dodsC/IMOS/ANMN/NRS/NRSROT/Temperature/IMOS_ANMN-NRS_TZ_20141216T080000Z_NRSROT_FV01_NRSROT-1412-SBE39-43_END-20150331T063000Z_C-20180508T001839Z.nc',
+    # 'http://thredds.aodn.org.au/thredds/dodsC/IMOS/ANMN/NRS/NRSROT/Temperature/IMOS_ANMN-NRS_TZ_20141216T080000Z_NRSROT_FV01_NRSROT-1412-SBE39-27_END-20150331T061500Z_C-20180508T001839Z.nc',
+    # 'http://thredds.aodn.org.au/thredds/dodsC/IMOS/ANMN/NRS/NRSROT/Temperature/IMOS_ANMN-NRS_TZ_20141216T080000Z_NRSROT_FV01_NRSROT-1412-TDR-2050-57_END-20150331T065000Z_C-20180508T001840Z.nc']
+
+    ## to test with a (large) WQM file
     files_to_aggregate = ['http://thredds.aodn.org.au/thredds/dodsC/IMOS/ANMN/NRS/NRSROT/Temperature/IMOS_ANMN-NRS_TZ_20141215T160000Z_NRSROT_FV01_NRSROT-1412-SBE39-33_END-20150331T063000Z_C-20180508T001839Z.nc',
     'http://thredds.aodn.org.au/thredds/dodsC/IMOS/ANMN/NRS/NRSROT/Temperature/IMOS_ANMN-NRS_TZ_20141216T080000Z_NRSROT_FV01_NRSROT-1412-SBE39-43_END-20150331T063000Z_C-20180508T001839Z.nc',
     'http://thredds.aodn.org.au/thredds/dodsC/IMOS/ANMN/NRS/NRSROT/Temperature/IMOS_ANMN-NRS_TZ_20141216T080000Z_NRSROT_FV01_NRSROT-1412-SBE39-27_END-20150331T061500Z_C-20180508T001839Z.nc',
-    'http://thredds.aodn.org.au/thredds/dodsC/IMOS/ANMN/NRS/NRSROT/Temperature/IMOS_ANMN-NRS_TZ_20141216T080000Z_NRSROT_FV01_NRSROT-1412-TDR-2050-57_END-20150331T065000Z_C-20180508T001840Z.nc']
+    'http://thredds.aodn.org.au/thredds/dodsC/IMOS/ANMN/NRS/NRSROT/Temperature/IMOS_ANMN-NRS_TZ_20141216T080000Z_NRSROT_FV01_NRSROT-1412-TDR-2050-57_END-20150331T065000Z_C-20180508T001840Z.nc',
+    'http://thredds.aodn.org.au/thredds/dodsC/IMOS/ANMN/NRS/NRSROT/Biogeochem_timeseries/IMOS_ANMN-NRS_BCKOSTUZ_20151208T080040Z_NRSROT_FV01_NRSROT-1512-WQM-24_END-20160411T021734Z_C-20180504T071457Z.nc']
 
 
     print(main_aggregator(files_to_agg=files_to_aggregate, var_to_agg=varname))
