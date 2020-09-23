faa587a3c9df4e1fee549a2789ba065d326d8134
Ana Berger <ana.berger@utas.edu.au>
2020-09-23 15:58:16 +1000

    improve-and-add-new-coding-to-save-log_file

--- DWM/update_abos_attributes_to_dwm.py
+++ DWM/update_abos_attributes_to_dwm.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python3
 """Rename ABOS files and attributes:
 
-As part of the task of renaming ABOS files to DWM, this script identifies the attributes that contain the words 
-"ABOS" and "Bluewater" (as part of the Australian Bluewater Observing System), changes them for "DWM"/"Deep Water 
-Moorings" and rename the files. The attributes 'history' and 'toolbox_input_file' are excluded from the search as we 
+As part of the task of renaming ABOS files to DWM, this script identifies the attributes that contain the words
+"ABOS" and "Bluewater" (as part of the Australian Bluewater Observing System), changes them for "DWM"/"Deep Water
+Moorings" and rename the files. The attributes 'history' and 'toolbox_input_file' are excluded from the search as we
 decided those attributes did not need to be changed. """
-
 
 import warnings
 import os
 import netCDF4 as nc
 import sys
-from typing import List
+import logging
 from glob import glob
 
 
 def find_att_with_str(ncobj: nc.Dataset, string: str = 'ABOS'):
-    """ Search in the attributes which one has 'ABOS' in it, excluding the 'history' and 'toolbox_input_file' 
+    """ Search in the attributes which one has 'ABOS' in it, excluding the 'history' and 'toolbox_input_file'
     attributes (we do not need to change these) """
-    attrs: List[str] = []
+    attrs = []
     for attname in ncobj.ncattrs():
         attvalue = getattr(ncobj, attname)
         got_requested_string = type(attvalue) is str and string in attvalue
@@ -27,7 +26,7 @@ def find_att_with_str(ncobj: nc.Dataset, string: str = 'ABOS'):
     return attrs
 
 
-def fix_abos_attributes(ncobj: nc.Dataset, namelist: List[str], old: str = 'ABOS', new: str = 'DWM'):
+def fix_abos_attributes(ncobj: nc.Dataset, namelist, old: str = 'ABOS', new: str = 'DWM'):
     """ Substitute 'ABOS' for 'DWM' in the attributes that contain 'ABOS' """
     for attname in namelist:
         attvalue = getattr(ncobj, attname)
@@ -38,18 +37,18 @@ def get_netcdf_files(folder: str):
     """ Get the nc files in the path given when calling the script in the command line """
     return glob(os.path.join(folder, '**/*.nc'), recursive=True)
 
- 
+
 def fix_abos_files(folder: str):
     """ Call the functions to replace attributes that contain 'ABOS' and/or 'Australia Bluewater Observing System') """
     files = get_netcdf_files(folder)
-
     for file in files:
         ncobj = nc.Dataset(file, 'a')
         abos_attributes = find_att_with_str(ncobj, 'ABOS')
         abos_blue_attributes = find_att_with_str(ncobj, 'Australia Bluewater Observing System')
         fix_abos_attributes(ncobj, abos_attributes, old='ABOS', new='DWM')
-        fix_abos_attributes(ncobj, abos_blue_attributes, old='Australia Bluewater Observing System',
-                            new='Deep Water Moorings')
+        fix_abos_attributes(ncobj, abos_blue_attributes, old='Australia Bluewater Observing System', new='Deep Water Moorings')
+        for attr_name in abos_attributes:
+            logging.info("New attribute: {}".format(getattr(ncobj, attr_name)))
         ncobj.close()
         rename_abos_file(file)
 
@@ -57,15 +56,23 @@ def fix_abos_files(folder: str):
 def rename_abos_file(old_filename: str):
     """ Rename the files substituting 'ABOS' for 'DWM' """
     if 'ABOS' not in old_filename:
-        warnings.warn(f"Rename requested for {old_filename}")
+        warnings.warn("Rename requested for: {}".format(old_filename))
         return
     else:
         new_filename = old_filename.replace("ABOS", "DWM")
-        print(f"Renaming {old_filename} -> {new_filename}")
+        print("Renamed file: {}".format(new_filename))
+        logging.info("Renamed file: {}".format(new_filename))
         os.rename(old_filename, new_filename)
 
 
 if __name__ == '__main__':
     dataDIR = sys.argv[1]
+    log_opts = {
+        'format': '%(asctime)s %(message)s',
+        'datefmt': '%m/%d/%Y %I:%M:%S %p',
+        'level': logging.INFO,
+        'filename': os.path.join(dataDIR, 'log_file_attrs_DWM.log')
+    }
+    logging.basicConfig(**log_opts)
     fix_abos_files(dataDIR)
 
