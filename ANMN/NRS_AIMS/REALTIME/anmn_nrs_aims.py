ef45896560c179da000cc7c00ede053c2422a326
lbesnard <laurent.besnard@utas.edu.au>
2021-03-15 17:26:07 +1100

    ANMN NRS - modify checksum value after AIMS web service modification (2)

--- ANMN/NRS_AIMS/REALTIME/anmn_nrs_aims.py
+++ ANMN/NRS_AIMS/REALTIME/anmn_nrs_aims.py
@@ -280,7 +280,7 @@ class AimsDataValidationTest(data_validation_test.TestCase):
         if sys.version_info[0] < 3:
             self.md5_expected_value = '76c9a595264a8173545b6dc0c518a280'
         else:
-            self.md5_expected_value = 'f87637a222b3857abdc238f177312967'
+            self.md5_expected_value = 'fd1a49f751dfd7569265069cf4cb91a6'
 
         self.md5_netcdf_value   = md5(self.netcdf_tmp_file_path)
 
