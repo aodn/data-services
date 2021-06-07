260084db6e6741a26ebbc06cdc45560de8a56e53
Leonardo Laiolo <leonardo.laiolo@utas.edu.au>
2021-06-07 10:36:32 +1000

    updating batchProcess.sh script so matlab script can run with new function and updated batchProcess.m so we have a record of where the calibration files are located

--- ANMN/acoustic/batchProcess.m
+++ ANMN/acoustic/batchProcess.m
@@ -1,22 +1,58 @@
 warning('off', 'MATLAB:HandleGraphics:noJVM');
+cd '/media/leonardo/April 2016/t3427IMOSNSW_E21/'
+NL_read_all_start_times(3427)
+NL_1Hz_PSD_with_plots4eMII('../Cal_2019_11/55DD2241.DAT', -90, -196, 't3427_start_times.mat', 5, 900)
+
+%cd '/media/leonardo/Acoustic1/public/PAPOR/3274/'
+%NL_read_all_start_times(3274)
+%NL_1Hz_PSD_with_plots4eMII('calibration/54E49F6D.DAT', -90, -196, 't3274_start_times.mat', 5, 900)
+
+%cd '/media/leonardo/Acoustic1/public/PAPCA/3376/'
+%NL_read_all_start_times(3376)
+%NL_1Hz_PSD_with_plots4eMII('calibration/529482AC.DAT', -90, -196, 't3376_start_times.mat', 5, 900)
+
+%cd '/home/leonardo/ANMN_Passive_Acoustics/PAKIM/3335/'
+%NL_read_all_start_times(3335)
+%NL_1Hz_PSD_with_plots4eMII('calibration/5D5D13F8.DAT', -90, -196, 't3335_start_times.mat', 5, 900)
+
+%cd '/media/leonardo/April 2016/t3505IMOSPortland_E47_6kHz/'
+%NL_read_all_start_times(3505)
+%NL_1Hz_PSD_with_plots4eMII('../Cal_2019_11/56EA4BD1.DAT', -90, -197.5, 't3505_start_times.mat', 5, 900)
+
+%cd '/media/leonardo/April 2016/t3444IMOSPerthCanyon_E24_6kHz/'
+%NL_read_all_start_times(3444)
+%NL_1Hz_PSD_with_plots4eMII('../Cal_2019_11/56720CC2.DAT', -90, -196, 't3444_start_times.mat', 5, 900)
+
+%cd '/media/leonardo/April 2016/t3444IMOSPerthCanyon_E24_6kHz/'
+%NL_read_all_start_times(3444)
+%NL_1Hz_PSD_with_plots4eMII('../Cal_2019_11/56720CC2.DAT', -90, -196, 't3444_start_times.mat', 5, 900)
+
+%cd '/media/leonardo/April 2016/t3501IMOSKI_E44_6kHz/'
+%NL_read_all_start_times(3501)
+%NL_1Hz_PSD_with_plots4eMII('../Cal_2019_11/5A3CBF99.DAT', -90, -198.1, 't3501_start_times.mat', 5, 900)
+
+%cd '/media/leonardo/IMOS_EMII/Acoustic/PAKIM/3250'
+%cd '/home/leonardo/PAKIM/3250'
+%NL_read_all_start_times(3250)
+%NL_1Hz_PSD_with_plots4eMII('calibration/5236BCF7.DAT', -90, -197.7, 't3250_start_times.mat', 5, 900)
 
 % 6193 files ~ 7728s ~ 2.15h
-disp('Processing PAPCA/2802');
-cd '/media/Seagate Expansion Drive/archive/PAPCA/2802'
-NL_read_all_start_times(2802)
-NL_1Hz_PSD_with_plots4eMII('../../cal/t2802_Calibration.DAT', -110, -197.6, 't2802_start_times.mat', 5, 900)
+%disp('Processing PAPCA/2802');
+%cd '/media/Seagate Expansion Drive/archive/PAPCA/2802'
+%NL_read_all_start_times(2802)
+%NL_1Hz_PSD_with_plots4eMII('../../cal/t2802_Calibration.DAT', -110, -197.6, 't2802_start_times.mat', 5, 900)
 
 % 18595 files ~ 51901s ~ 14.4h
-disp('Processing PAPCA/2823');
-cd '/media/Seagate Expansion Drive/archive/PAPCA/2823'
-NL_read_all_start_times(2823)
-NL_1Hz_PSD_with_plots4eMII('../../cal/t2823_Calibration.DAT', -110, -197.6, 't2823_start_times.mat', 5, 900)
+%disp('Processing PAPCA/2823');
+%cd '/media/Seagate Expansion Drive/archive/PAPCA/2823'
+%NL_read_all_start_times(2823)
+%NL_1Hz_PSD_with_plots4eMII('../../cal/t2823_Calibration.DAT', -110, -197.6, 't2823_start_times.mat', 5, 900)
 
 % 26339 files ~ 62734s ~ 17.4h
-disp('Processing PAPCA/2962');
-cd '/media/Seagate Expansion Drive/archive/PAPCA/2962'
-NL_read_all_start_times(2962)
-NL_1Hz_PSD_with_plots4eMII('../../cal/t2962_Calibration.DAT', -90, -197.9, 't2962_start_times.mat', 5, 900)
+%disp('Processing PAPCA/2962');
+%cd '/media/Seagate Expansion Drive/archive/PAPCA/2962'
+%NL_read_all_start_times(2962)
+%NL_1Hz_PSD_with_plots4eMII('../../cal/t2962_Calibration.DAT', -90, -197.9, 't2962_start_times.mat', 5, 900)
 
 %  files ~ h
 % disp('Processing PAPCA/3004');
@@ -25,10 +61,10 @@ NL_1Hz_PSD_with_plots4eMII('../../cal/t2962_Calibration.DAT', -90, -197.9, 't296
 % NL_1Hz_PSD_with_plots4eMII('../../cal/t3004_Calibration.DAT', -90, -197.9, 't3004_start_times.mat', 5, 900)
 
 % 21982 files ~ 63438s ~ 17.6h
-disp('Processing PAPOR/2846');
-cd '/media/Seagate Expansion Drive/archive/PAPOR/2846'
-NL_read_all_start_times(2846)
-NL_1Hz_PSD_with_plots4eMII('../../cal/t2846_Calibration.DAT', -90, -197.9, 't2846_start_times.mat', 5, 900)
+%disp('Processing PAPOR/2846');
+%cd '/media/Seagate Expansion Drive/archive/PAPOR/2846'
+%NL_read_all_start_times(2846)
+%NL_1Hz_PSD_with_plots4eMII('../../cal/t2846_Calibration.DAT', -90, -197.9, 't2846_start_times.mat', 5, 900)
 
 %  files ~ h
 % disp('Processing PAPOR/3102');
