9ed7464b282431335bb5b800865742bb5a758e1e
lbesnard <laurent.besnard@utas.edu.au>
2021-09-13 12:25:29 +1000

    (Feat) add download option to s3_ls_prefix python script

--- lib/python/s3_ls_prefix.py
+++ lib/python/s3_ls_prefix.py
@@ -6,6 +6,7 @@ import argparse
 import boto3
 import sys
 import os
+import urllib
 
 EXAMPLE_TEXT = """
 Requirements:
@@ -15,10 +16,12 @@ Requirements:
      copy and rename to ~/.aws/config
 
 Examples:
-  ./s3_ls_prefix.py -p 'Defence_Technology_Agency-New_Zealand/Waverider_Buoys_C-20200615T000000Z/' \n
+  ./s3_ls_prefix.py -p 'Defence_Technology_Agency-New_Zealand/Waverider_Buoys_C-20200615T000000Z/'
   ./s3_ls_prefix.py -b imos-test-data -p 'Defence_Technology_Agency'
+  ./s3_ls_prefix.py -p IMOS/ACORN/gridded_1h-avg-current-map_QC/NWA/2021/07/ -o /tmp/nwa_dm
 """
 
+URL_PREFIX="https://s3-ap-southeast-2.amazonaws.com"
 
 def _s3_ls_bucket_prefix(bucket, prefix):
     s3 = boto3.resource('s3')
@@ -31,13 +34,27 @@ def _s3_ls_bucket_prefix(bucket, prefix):
     return s3_obj
 
 
+def _download_s3_obj_flat(bucket, s3_obj_ls, output_dir):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    for f in s3_obj_ls:
+        url='{prefix_url}/{bucket}/{object_path}'.format(prefix_url=URL_PREFIX,
+                                                        bucket=bucket,
+                                                        object_path=f)
+        output_file = os.path.join(output_dir, os.path.basename(f))
+
+        print('Downloading {f}'.format(f=f), file=sys.stdout)
+        urllib.request.urlretrieve(url, output_file)
+
+
 def args():
     """
     define the script arguments
     :return: vargs
     """
     parser = argparse.ArgumentParser(epilog=EXAMPLE_TEXT,
-                                     description='script to list s3 object from an AODN bucket to stdout',
+                                     description='script to list/download s3 object from an AODN bucket to stdout',
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument("-p", "--prefix",
                         type=str,
@@ -47,6 +64,10 @@ def args():
                         default='imos-data',
                         type=str,
                         help="s3 bucket - default value is 'imos-data'")
+
+    parser.add_argument("-o", "--output-dir",
+                        type=str,
+                        help="download listed objects to --output-dir")
 
     return parser.parse_args()
 
@@ -59,6 +80,10 @@ if __name__ == '__main__':
     bucket = vargs.bucket
     os.environ['AWS_PROFILE'] = 'production-projectofficer'
 
-    s3_obj = _s3_ls_bucket_prefix(bucket, prefix)
-    for f in s3_obj:
-        print(f, file=sys.stdout)
+    s3_obj_ls = _s3_ls_bucket_prefix(bucket, prefix)
+    if vargs.output_dir:
+        _download_s3_obj_flat(bucket, s3_obj_ls, vargs.output_dir)
+    else:
+        for f in s3_obj_ls:
+            print(f, file=sys.stdout)
+
