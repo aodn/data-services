0c32406c8bee6207879d718cd529697841ad6772
bpasquer <benedicte.pasquer@utas.edu.au>
2023-01-18 13:46:18 +1100

    S3 listing of object versions and download options

--- lib/python/s3_ls_prefix.py
+++ lib/python/s3_ls_prefix.py
@@ -7,6 +7,7 @@ import boto3
 import sys
 import os
 import urllib
+from botocore.exceptions import ClientError
 
 EXAMPLE_TEXT = """
 Requirements:
@@ -14,11 +15,12 @@ Requirements:
   2) download file
      https://github.com/aodn/cloud-deploy/blob/master/sample-config/aws/config_projectofficer
      copy and rename to ~/.aws/config
-
 Examples:
   ./s3_ls_prefix.py -p 'Defence_Technology_Agency-New_Zealand/Waverider_Buoys_C-20200615T000000Z/'
   ./s3_ls_prefix.py -b imos-test-data -p 'Defence_Technology_Agency'
   ./s3_ls_prefix.py -p IMOS/ACORN/gridded_1h-avg-current-map_QC/NWA/2021/07/ -o /tmp/nwa_dm
+   ./s3_ls_prefix.py -l CSIRO/Climatology/Ocean_Acidification/OA_Reconstruction.nc
+  ./s3_ls_prefix.py -v CSIRO/Climatology/Ocean_Acidification/OA_Reconstruction.nc h40zqcPFdSR1POV5Ihon3mH8nvt7SkgU
 """
 
 URL_PREFIX="https://s3-ap-southeast-2.amazonaws.com"
@@ -47,6 +49,22 @@ def _download_s3_obj_flat(bucket, s3_obj_ls, output_dir):
         print('Downloading {f}'.format(f=f), file=sys.stdout)
         urllib.request.urlretrieve(url, output_file)
 
+def _list_object_version(bucket, prefix):
+   s3 = boto3.client('s3')
+   try:
+      result = s3.list_object_versions(Bucket=bucket, Prefix=prefix)
+   except ClientError as e:
+      raise Exception("boto3 client error in list_all_objects_version function: " + e.__str__())
+   except Exception as e:
+      raise Exception("Unexpected error in list_all_objects_version function of s3 helper: " + e.__str__())
+
+   return result
+
+def _download_file_version(bucket, prefix, versionid):
+    s3 = boto3.client('s3')
+    filename = os.path.basename(prefix)
+    s3.download_file(bucket, prefix, filename, ExtraArgs={'VersionId': versionid})
+
 
 def args():
     """
@@ -69,21 +87,45 @@ def args():
                         type=str,
                         help="download listed objects to --output-dir")
 
+    parser.add_argument("-l", "--listversion",
+                        type=str,
+                        help="list object version --listversion")
+
+    parser.add_argument("-v", "--versionid", nargs=2 ,
+                        type=str,
+                        help="download the versionid of a given object. "
+                             "Example IMOS/ANFOG/"
+                             "IMOS_ANFOG_BCEOPSTUVN_20151021T035731Z_SL416_FV01_timeseries_END-20151027T015319Z.nc"
+                             " pAo_g3MuONfNf5wIgeq0ff5WHswj_Wbd --versionid")
+
     return parser.parse_args()
 
 
 if __name__ == '__main__':
 
     vargs = args()
-
-    prefix = vargs.prefix
+    if vargs.prefix:
+        prefix = vargs.prefix
     bucket = vargs.bucket
     os.environ['AWS_PROFILE'] = 'production-projectofficer'
 
-    s3_obj_ls = _s3_ls_bucket_prefix(bucket, prefix)
     if vargs.output_dir:
+        s3_obj_ls = _s3_ls_bucket_prefix(bucket, prefix)
         _download_s3_obj_flat(bucket, s3_obj_ls, vargs.output_dir)
+    elif vargs.listversion:
+        prefix = vargs.listversion
+        res =_list_object_version(bucket, prefix)
+        print(res['Prefix'])
+        for values in res.values():
+            if(isinstance(values, list)):
+                for value in values:
+                    print('VersionId:',value['VersionId'],'Lastmodified:', value['LastModified'])
+        print('Number of versions', len(res['Versions']))
+    elif vargs.versionid:
+        object = vargs.versionid[0]
+        versionid =  vargs.versionid[1]
+        _download_file_version(bucket, object, versionid)
     else:
+        s3_obj_ls = _s3_ls_bucket_prefix(bucket, prefix)
         for f in s3_obj_ls:
             print(f, file=sys.stdout)
-
