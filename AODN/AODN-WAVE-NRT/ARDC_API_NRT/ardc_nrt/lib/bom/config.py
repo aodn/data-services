8d0f2210dfe0e24b5128f66045cc76c0616e45c3
bpasquer <benedicte.pasquer@utas.edu.au>
2023-07-07 12:44:46 +1000

    Wave- switch over new BOM WFS URL

--- AODN/AODN-WAVE-NRT/ARDC_API_NRT/ardc_nrt/lib/bom/config.py
+++ AODN/AODN-WAVE-NRT/ARDC_API_NRT/ardc_nrt/lib/bom/config.py
@@ -1,4 +1,4 @@
-url_prefix = 'http://spatial.bom.gov.au/cgi-bin/mapserver/aodn/aodn?service=WFS&version=1.0.0'
+url_prefix = 'https://spatial.bom.gov.au/cgi-bin/mapserver/aodn/aodn?service=WFS&version=1.0.0'
 typename='IDY69100'
 conf_dirpath = 'config/bom'
 mapped_source_id = {
