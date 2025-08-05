483468749b96ef323ab4db1659b73b6b8a5baa41
lbesnard <laurent.besnard@utas.edu.au>
2025-08-05 10:27:30 +1000

    Feat: surface waves altimetry - add SWOT

--- SRS/SRS_SURFACE_WAVES/srs_sar_altimeter_pipeline_trigger.sh
+++ SRS/SRS_SURFACE_WAVES/srs_sar_altimeter_pipeline_trigger.sh
@@ -4,22 +4,22 @@
 # requires fd
 # author laurent besnard
 
-fd_bin=`command -v fd`
+fd_bin=$(command -v fd)
 [ -z $fd_bin ] && echo "please install fd to use this script" && exit 0
 
 SRS_ALTI_DIR=$INCOMING_DIR/SRS/SURFACE_WAVES/altimeter/incoming
 SRS_ALTI_MANIFEST_DIR=$SRS_ALTI_DIR/manifest_dir
 
 declare -a sat_list_array
-sat_list_array=(CRYOSAT-2 CFOSAT ENVISAT ERS-1 ERS-2 GEOSAT GFO HY-2 HY-2B JASON-1 \
-    JASON-2 JASON-3 SARAL SENTINEL-3A SENTINEL-3B SENTINEL-6A TOPEX)
+sat_list_array=(CRYOSAT-2 CFOSAT ENVISAT ERS-1 ERS-2 GEOSAT GFO HY-2 HY-2B JASON-1
+  JASON-2 JASON-3 SARAL SENTINEL-3A SENTINEL-3B SENTINEL-6A TOPEX SWOT)
 
 for i in "${sat_list_array[@]}"; do
-    fd -t f -a -e nc _"$i"_ $SRS_ALTI_DIR > $SRS_ALTI_MANIFEST_DIR/"$i".manifest
-    split -d -l1024 $SRS_ALTI_MANIFEST_DIR/"$i".manifest \
-        "$SRS_ALTI_MANIFEST_DIR"/srs_altimeter.$i. \
-        --additional-suffix=.manifest
-    rm -f $SRS_ALTI_MANIFEST_DIR/"$i".manifest
+  fd -t f -a -e nc _"$i"_ $SRS_ALTI_DIR >$SRS_ALTI_MANIFEST_DIR/"$i".manifest
+  split -d -l1024 $SRS_ALTI_MANIFEST_DIR/"$i".manifest \
+    "$SRS_ALTI_MANIFEST_DIR"/srs_altimeter.$i. \
+    --additional-suffix=.manifest
+  rm -f $SRS_ALTI_MANIFEST_DIR/"$i".manifest
 done
 
 # manually done
